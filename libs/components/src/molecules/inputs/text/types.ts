7cf9c9d2c6d3cbd06c3189533a753a95f19ff93a
RohendoJ <rohendoj3134.github@gmail.com>
2023-06-27 16:01:57 +0700

    Add global components | textfield

---
+++ libs/components/src/molecules/inputs/text/types.ts
@@ -0,0 +1,27 @@
+import { ChangeEventHandler, ReactNode } from "react";
+import { FieldValues, UseControllerProps } from "react-hook-form";
+import { StaticImageData } from "next/image";
+
+export type TTextFieldProps<T extends FieldValues> = UseControllerProps<T> & {
+  type?: "text" | "password" | "email" | "number" | "date";
+  label?: string;
+  name: string;
+  placeholder?: string;
+  required?: boolean;
+  disabled?: boolean;
+  value?: string;
+  className?: string;
+  labelClassName?: string;
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+  status?: "success" | "error" | "warning" | "none";
+  message?: string;
+  variant: "lg" | "md" | "sm";
+  icon?: ReactNode | StaticImageData;
+  prepend?: ReactNode;
+  append?: ReactNode;
+  prependColor?: string;
+  appendColor?: string;
+  hint?: string;
+  isTextArea?: boolean;
+  textAreaRow?: number;
+};
