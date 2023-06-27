851affee8472ca33e3905fdf83f04cf672f30da9
Rohendo <108710826+RohendoJ@users.noreply.github.com>
2023-06-27 17:22:50 +0700

    Add global components | textfield (#35)

--- apps/pmb/app/page.tsx
+++ apps/pmb/app/page.tsx
@@ -1,18 +1,63 @@
-import { Button } from '@uninus/components';
-import {
-  BUTTON_SIZE,
-  BUTTON_VARIANT,
-} from 'libs/components/src/atoms/button/enums';
-import { NextPage } from 'next';
+'use client';
 import { Fragment, ReactElement } from 'react';
+import { TextField, Button } from '@uninus/components';
+import { BUTTON_VARIANT } from 'libs/components/src/atoms/button/enums';
+import { useForm } from 'react-hook-form';
 
-const LandingPage: NextPage = (): ReactElement => {
+const LandingPage = (): ReactElement => {
+  const { control } = useForm({
+    defaultValues: {
+      name: 'text field',
+    },
+  });
   return (
     <Fragment>
       <Button href="https://litera.uninus.ac.id/uninus/login.jsp">
         LITERA
       </Button>
       <Button variant={BUTTON_VARIANT.ERROR}>Waduh</Button>
+      <TextField
+        required
+        control={control}
+        name={'name'}
+        label="Nama Lengkap"
+        placeholder="Masukkan Nama Anda"
+        variant="lg"
+        message="Berhasil Memvalidasi"
+        status="success"
+      />
+      <TextField
+        required
+        control={control}
+        name={'name'}
+        label="Nama Lengkap"
+        placeholder="Masukkan Nama Anda"
+        variant="md"
+        message="Gagal tidak valid"
+        status="error"
+      />
+      <TextField
+        required
+        control={control}
+        name={'name'}
+        label="Nama Lengkap"
+        placeholder="Masukkan Nama Anda"
+        variant="sm"
+        message="Kurang banyak"
+        status="warning"
+      />
+      <TextField
+        required
+        type="password"
+        control={control}
+        name={'name'}
+        label="Kata Sandi"
+        placeholder="Masukkan Nama Anda"
+        variant="lg"
+        message="Berhasil Memvalidasi"
+        status="none"
+        hint="Password setidaknya ada 8 karakter"
+      />
     </Fragment>
   );
 };
