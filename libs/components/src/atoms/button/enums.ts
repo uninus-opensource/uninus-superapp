d781a196ec4f021520fd24d2ccdc588049209843
EL5225 <luthfiramadhan155@gmail.com>
2023-06-27 13:53:03 +0700

    feature: button variant

---
+++ libs/components/src/atoms/button/enums.ts
@@ -0,0 +1,14 @@
+export enum BUTTON_VARIANT {
+  PRIMARY = 'primary',
+  PRIMARY_OUTLINE = 'primary-outline',
+  ERROR = 'error',
+  ERROR_OUTLINE = 'error-outline',
+  WARNING = 'warning',
+  WARNING_OUTLINE = 'warning-outline',
+}
+
+export enum BUTTON_SIZE {
+  SM = 'sm',
+  MD = 'md',
+  LG = 'lg',
+}
---
+++ libs/components/src/atoms/button/interface.ts
@@ -0,0 +1,9 @@
+import { ButtonHTMLAttributes } from 'react';
+import { BUTTON_SIZE, BUTTON_VARIANT } from './enums';
+
+export interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: BUTTON_SIZE;
+  variant?: BUTTON_VARIANT;
+  loading?: boolean;
+  href?: string;
+}
