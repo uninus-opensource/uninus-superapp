1c4de51c752751af2e5ec83aeda2a3cdf3e254c2
Akhyar.Azamta <bebyraska14@gmail.com>
2023-06-29 03:16:21 +0700

    feature: radio button react-hook-form

--- apps/pmb/app/page.tsx
+++ apps/pmb/app/page.tsx
@@ -1,55 +1,113 @@
-'use client'
-import React from 'react';
-import { useForm, FieldError, FieldValues, UseFormRegister } from 'react-hook-form';
+'use client';
+import { Fragment, ReactElement } from 'react';
+import { useForm } from 'react-hook-form';
+import { Button, CheckBox, Navbar, SelectField } from '@uninus/components';
 
-type RadioButtonProps = {
-  register: UseFormRegister<FieldValues>;
-  error?: FieldError;
-  name: string;
-  value: string;
-  label: string;
-};
-
-const RadioButton: React.FC<RadioButtonProps> = ({ register, error, name, value, label }) => {
-  return (
-    <div>
-      <input type="radio" value={value} {...register(name)} />
-      <label>{label}</label>
-      {error && <p>{error.message}</p>}
-    </div>
-  );
-};
-
-const MyForm: React.FC = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
-
-  const onSubmit = (data: any) => {
-    alert(data.myRadioField);
-  };
+const LandingPage = (): ReactElement => {
+  const { control } = useForm({
+    defaultValues: {
+      checkboxField: false,
+    },
+  });
 
   return (
-    <div className='flex justify-center items-center h-screen'>
-    <form onSubmit={handleSubmit(onSubmit)}>
-        
-      <RadioButton
-        register={register}
-        error={errors.myRadioField as FieldError}
-        name="myRadioField"
-        value="option1"
-        label="Opsi 1"
-      />
-      <RadioButton
-        register={register}
-        error={errors.myRadioField as FieldError}
-        name="myRadioField"
-        value="option2"
-        label="Opsi 2"
+    <Fragment>
+      <Navbar />
+      <div className="flex flex-col justify-center items-center h-screen gap-4">
+        <CheckBox
+          control={control}
+          name={'checkboxField'}
+          required
+          variant="primary"
+          size="sm"
+          labelSize="sm"
+          label="Setuju"
+          message="Valid"
         />
 
-      <button type="submit">Submit</button>
-    </form>
+        <CheckBox
+          control={control}
+          name={'checkboxField'}
+          required
+          variant="error"
+          size="md"
+          labelSize="md"
+          label="Setuju"
+          message="Valid"
+        />
+
+        <CheckBox
+          control={control}
+          name={'checkboxField'}
+          required
+          variant="warning"
+          size="lg"
+          labelSize="lg"
+          label="Setuju"
+          message="Valid"
+        />
+        <div className="flex gap-2">
+          <div className="mb-2">
+            <SelectField
+              name="peran"
+              label="Peran"
+              size="md"
+              placeholder="pilih peran"
+              status="none"
+              options={['Mahasiswa', 'Dosen', 'Staff']}
+            />
+          </div>
+          <SelectField
+            name="peran"
+            label="Peran"
+            size="md"
+            placeholder="pilih peran"
+            status="error"
+            options={['Mahasiswa', 'Dosen', 'Staff']}
+            message="error sample"
+          />
+          <SelectField
+            name="peran"
+            label="Peran"
+            size="md"
+            placeholder="pilih peran"
+            status="success"
+            options={['Mahasiswa', 'Dosen', 'Staff']}
+            message="success sample"
+          />
         </div>
+        <div className="flex gap-4">
+          <Button
+            href="https://litera.uninus.ac.id/uninus/login.jsp"
+            variant="primary"
+            size="sm"
+            width="w-28"
+            height="h-8"
+          >
+            Litera
+          </Button>
+          <Button
+            href="https://www.facebook.com/"
+            variant="error"
+            size="md"
+            width="w-28"
+            height="h-8"
+          >
+            Facebook
+          </Button>
+          <Button
+            href="https://www.google.com/"
+            variant="warning"
+            size="lg"
+            width="w-28"
+            height="h-8"
+          >
+            Google
+          </Button>
+        </div>
+      </div>
+    </Fragment>
   );
 };
 
-export default MyForm;
+export default LandingPage;
