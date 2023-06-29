4ea9cbbd9a18f920e7b517d577d183babb286819
Akhyar.Azamta <bebyraska14@gmail.com>
2023-06-29 21:48:56 +0700

    react-table

---
+++ libs/components/src/organisms/table/defaultData.ts
@@ -0,0 +1,28 @@
+import { Datas } from "./type";
+
+export const defaultData: Datas[] = [
+  {
+    firstName: 'joko',
+    lastName: 'wi',
+    age: 3,
+    visits: 100,
+    status: 'President Konoha',
+    progress: 0,
+  },
+  {
+    firstName: 'Ban',
+    lastName: 'teng',
+    age: 3,
+    visits: 100,
+    status: 'Lord',
+    progress: 0,
+  },
+  {
+    firstName: 'Me',
+    lastName: 'rah',
+    age: 3,
+    visits: 100,
+    status: 'brave',
+    progress: 0,
+  },
+];
