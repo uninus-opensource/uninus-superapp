0ba703e950da642745af5b5ecc6df64185e1b802
RohendoJ <rohendoj3134.github@gmail.com>
2023-07-02 00:21:23 +0700

    feature: add reset password login pmb

---
+++ apps/pmb/app/login/reset-password/page.tsx
@@ -0,0 +1,46 @@
+'use client';
+import { FC, ReactElement } from 'react';
+import { Button, TextField } from '@uninus/components';
+import { useForm } from 'react-hook-form';
+
+const ResetPassword: FC = (): ReactElement => {
+  const { control } = useForm({
+    defaultValues: {
+      newPassword: '',
+      confirmPassword: '',
+    },
+  });
+
+  return (
+    <section>
+      <div className="h-screen flex flex-col justify-center items-center gap-2">
+        <div className="w-[50vw] md:w-[40vw] lg:w-[30vw] xl:w-[25vw]">
+          <h1 className="font-bold text-[1.7rem]">Ubah Password</h1>
+          <div className="mt-5">
+            <TextField
+              name="newPassword"
+              variant="sm"
+              type="password"
+              label="Password baru"
+              control={control}
+              required
+            />
+          </div>
+          <TextField
+            name="newPassword"
+            variant="sm"
+            type="password"
+            label="Konfirmasi password"
+            control={control}
+            required
+          />
+          <div className="mt-5">
+            <Button width="w-full">Reset Password</Button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ResetPassword;
