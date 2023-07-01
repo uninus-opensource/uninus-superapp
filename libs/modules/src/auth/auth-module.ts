bdb168b2030039158d6b402f9c096ca5ad033f99
sugiartofahmi <sugiartofahmi@gmail.com>
2023-07-01 07:31:20 +0700

    improvement: restructure project

--- libs/modules/src/auth/auth-module.ts
+++ libs/modules/src/auth/auth-module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from '../prisma/index';
-import { AuthService } from '@uninus/services';
-import { AuthController } from '@uninus/controllers';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { AuthController } from '@uninus/controllers';
+import { AuthService } from '@uninus/services';
+import { PmbModule } from '../pmb';
+import { PrismaModule } from '../prisma/index';
 
 @Module({
-  imports: [PrismaModule, JwtModule, PassportModule],
+  imports: [PrismaModule, JwtModule, PassportModule, PmbModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
