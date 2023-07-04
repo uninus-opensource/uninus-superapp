577fc4d852d0d01600078f00ed3a0f5e671b3218
Akhyar.Azamta <bebyraska14@gmail.com>
2023-07-04 13:40:33 +0700

    feature: accordions

---
+++ libs/components/src/organisms/accordion/type.ts
@@ -0,0 +1,5 @@
+export type AccordionItem = {
+  titles: string[];
+  contents: string[][];
+  title?: string;
+};
--- libs/components/src/organisms/index.ts
+++ libs/components/src/organisms/index.ts
@@ -1,2 +1,3 @@
 export * from './navbar';
 export * from './modal';
+export * from './accordion';
