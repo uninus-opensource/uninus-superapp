d4239dc400c00a7b1c25c08689fd616cf2185e13
Akhyar-Azamta <45706780+AkhyarAzamta@users.noreply.github.com>
2023-07-06 02:45:01 +0700

    feature: accordions (#157)
    * feature: accordions
    
    * feature: accordion

---
+++ libs/components/src/organisms/accordion/type.ts
@@ -0,0 +1,6 @@
+export type AccordionItem = {
+  header: string;
+  titles: string[];
+  contents: string[][];
+  title?: string;
+};
--- libs/components/src/organisms/index.ts
+++ libs/components/src/organisms/index.ts
@@ -1,5 +1,8 @@
 export * from './navbar';
 export * from './modal';
+export * from './accordion';
 export * from './hero-banner';
+
+
 export * from './card';
 export * from './tab';
