e833b4dc341c0d0932dc8a818f34673477abd03e
RohendoJ <rohendoj3134.github@gmail.com>
2023-07-05 20:42:01 +0700

    improvement: footer with link

--- libs/components/src/molecules/footer/index.tsx
+++ libs/components/src/molecules/footer/index.tsx
@@ -16,6 +16,7 @@ import {
 } from '@ant-design/icons';
 import NeoUninusIcon from '../../atoms/illustrations/neouninus/Neo-Uninus.png';
 import hybridUniversity from '../../atoms/illustrations/hybriduniversity/hybrid-university.png';
+import Link from 'next/link';
 
 export const Footer: NextPage = (): ReactElement => {
   return (
@@ -38,29 +39,58 @@ export const Footer: NextPage = (): ReactElement => {
           {/* line */}
 
           <section className="w-[33.3%] flex flex-col justify-center gap-5">
-            <div className="flex items-center justify-center gap-3">
-              <GlobalOutlined style={{ fontSize: 25, color: 'white' }} />
-              <h3 className="font-bebasNeue font-bold text-2xl text-primary-white">
-                SITUS RESMI UNINUS
-              </h3>
+            {/* Situs resmi uninus */}
+            <div className="group hover:cursor-pointer">
+              <Link href="https://uninus.ac.id/" target="_blank">
+                <div className="flex items-center justify-center gap-3 group-hover:scale-95 duration-300 ease-in-out">
+                  <GlobalOutlined style={{ fontSize: 25, color: 'white' }} />
+                  <h3 className="font-bebasNeue font-bold text-2xl text-primary-white ">
+                    SITUS RESMI UNINUS
+                  </h3>
+                </div>
+              </Link>
             </div>
-            <div className="flex items-center ml-[18%] gap-3">
-              <FileTextOutlined style={{ fontSize: 25, color: 'white' }} />
-              <h3 className="font-bebasNeue font-bold text-2xl text-primary-white">
-                UNDUH BROSUR
-              </h3>
+
+            {/* Unduh Brosur */}
+            <div className="group hover:cursor-pointer">
+              <div
+                className="flex items-center ml-[18%] gap-3 group-hover:scale-95 duration-300 ease-in-out"
+                onClick={() => alert('Brosurnya belum ada')}
+              >
+                <FileTextOutlined style={{ fontSize: 25, color: 'white' }} />
+                <h3 className="font-bebasNeue font-bold text-2xl text-primary-white">
+                  UNDUH BROSUR
+                </h3>
+              </div>
             </div>
-            <div className="flex items-center ml-[18%] gap-3">
-              <EnvironmentOutlined style={{ fontSize: 25, color: 'white' }} />
-              <h3 className="font-bebasNeue font-bold text-2xl text-primary-white">
-                LOKASI KAMPUS
-              </h3>
+
+            {/* Lokasi Kampus */}
+            <div className="group hover:cursor-pointer">
+              <Link
+                href="https://goo.gl/maps/pdvgVFaFmzLhUeSs5"
+                target="_blank"
+              >
+                <div className="flex items-center ml-[18%] gap-3 group-hover:scale-95 duration-300 ease-in-out">
+                  <EnvironmentOutlined
+                    style={{ fontSize: 25, color: 'white' }}
+                  />
+                  <h3 className="font-bebasNeue font-bold text-2xl text-primary-white">
+                    LOKASI KAMPUS
+                  </h3>
+                </div>
+              </Link>
             </div>
-            <div className="flex items-center ml-[18%] gap-3">
-              <PhoneOutlined style={{ fontSize: 25, color: 'white' }} />
-              <h3 className="font-bebasNeue font-bold text-2xl text-primary-white">
-                HUBUNGI KAMI
-              </h3>
+
+            {/* Hubungi Kami */}
+            <div className="group hover:cursor-pointer">
+              <Link href="tel:+628112161530" target="_blank">
+                <div className="flex items-center ml-[18%] gap-3 group-hover:scale-95 duration-300 ease-in-out">
+                  <PhoneOutlined style={{ fontSize: 25, color: 'white' }} />
+                  <h3 className="font-bebasNeue font-bold text-2xl text-primary-white">
+                    HUBUNGI KAMI
+                  </h3>
+                </div>
+              </Link>
             </div>
           </section>
 
@@ -77,20 +107,43 @@ export const Footer: NextPage = (): ReactElement => {
                 height={53}
               />
             </div>
-            <div className="flex items-center ml-[18%] gap-3">
-              <EnvironmentFilled style={{ fontSize: 25, color: 'white' }} />
-              <h3 className="text-xs text-primary-white w-[178px] h-[45px]">
-                Jl. Soekarno Hatta No.530, Sekejati, Kec. Buahbatu, Kota
-                Bandung, Jawa Barat 40286
-              </h3>
+
+            {/* Alamat */}
+            <div className="group hover:cursor-pointer">
+              <Link
+                href="https://goo.gl/maps/pdvgVFaFmzLhUeSs5"
+                target="_blank"
+              >
+                <div className="flex items-center ml-[18%] gap-3 group-hover:scale-95 duration-300 ease-in-out">
+                  <EnvironmentFilled style={{ fontSize: 25, color: 'white' }} />
+                  <h3 className="text-xs text-primary-white w-[178px] h-[45px]">
+                    Jl. Soekarno Hatta No.530, Sekejati, Kec. Buahbatu, Kota
+                    Bandung, Jawa Barat 40286
+                  </h3>
+                </div>
+              </Link>
             </div>
-            <div className="flex items-center ml-[18%] gap-3">
-              <ContactsFilled style={{ fontSize: 25, color: 'white' }} />
-              <h3 className="text-xs text-primary-white">0811-2161-530</h3>
+
+            {/* Kontak */}
+            <div className="group hover:cursor-pointer">
+              <Link href="tel:+628112161530" target="_blank">
+                <div className="flex items-center ml-[18%] gap-3 group-hover:scale-95 duration-300 ease-in-out">
+                  <ContactsFilled style={{ fontSize: 25, color: 'white' }} />
+                  <h3 className="text-xs text-primary-white">0811-2161-530</h3>
+                </div>
+              </Link>
             </div>
-            <div className="flex items-center ml-[18%] gap-3">
-              <MailFilled style={{ fontSize: 25, color: 'white' }} />
-              <h3 className="text-xs text-primary-white">humas@uninus.ac.id</h3>
+
+            {/* Email */}
+            <div className="group hover:cursor-pointer">
+              <Link href="mailto:humas@uninus.ac.id" target="_blank">
+                <div className="flex items-center ml-[18%] gap-3 group-hover:scale-95 duration-300 ease-in-out">
+                  <MailFilled style={{ fontSize: 25, color: 'white' }} />
+                  <h3 className="text-xs text-primary-white">
+                    humas@uninus.ac.id
+                  </h3>
+                </div>
+              </Link>
             </div>
           </section>
         </div>
@@ -111,10 +164,43 @@ export const Footer: NextPage = (): ReactElement => {
           <p className="font-bebasNeue text-xl leading-6 text-grayscale-1 font-extrabold">
             © NEO UNIVERSITAS ISLAM NUSANTARA 2023
           </p>
+
+          {/* Facebook */}
           <div className="flex gap-5">
-            <FacebookFilled style={{ fontSize: 25, color: 'white' }} />
-            <YoutubeFilled style={{ fontSize: 25, color: 'white' }} />
-            <InstagramFilled style={{ fontSize: 25, color: 'white' }} />
+            <div className="group cursor-pointer">
+              <div className="group-hover:scale-90 duration-300 ease-in-out">
+                <Link
+                  href="https://www.facebook.com/uninusbandung"
+                  target="_blank"
+                >
+                  <FacebookFilled style={{ fontSize: 25, color: 'white' }} />
+                </Link>
+              </div>
+            </div>
+
+            {/* Youtube */}
+            <div className="group cursor-pointer">
+              <div className="group-hover:scale-90 duration-300 ease-in-out">
+                <Link
+                  href="https://www.youtube.com/@uninusbandung"
+                  target="_blank"
+                >
+                  <YoutubeFilled style={{ fontSize: 25, color: 'white' }} />
+                </Link>
+              </div>
+            </div>
+
+            {/* Instagram */}
+            <div className="group cursor-pointer">
+              <div className="group-hover:scale-90 duration-300 ease-in-out">
+                <Link
+                  href="https://instagram.com/uninusbandung"
+                  target="_blank"
+                >
+                  <InstagramFilled style={{ fontSize: 25, color: 'white' }} />
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </section>
