5b2c04115523acd12e6bfe26875ab4b71ec238d0
dika841 <randikaa1999@gmail.com>
2023-07-07 03:16:03 +0700

    Improvement : Update slicing Halaman Register dan Update komponen TextField

--- apps/pmb/app/register/page.tsx
+++ apps/pmb/app/register/page.tsx
@@ -1,12 +1,15 @@
 'use client';
-import { TextField, Button, CheckBox } from '@uninus/components';
-import { Fragment, ReactElement } from 'react';
+import { TextField, Button } from '@uninus/components';
+import { ReactElement } from 'react';
 import { useForm } from 'react-hook-form';
 import { NextPage } from 'next';
 import { TRegisterRequest } from '@uninus/entities';
 import { useRegister } from '@uninus/modules-fe';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
+import Image from 'next/image';
+import neoLogo from '../illustrations/Neo-Uninus2.png';
+import hybridLogo from '../illustrations/hybrid-university.png';
 
 const Register: NextPage = (): ReactElement => {
   const { control, handleSubmit } = useForm<
@@ -40,71 +43,93 @@ const Register: NextPage = (): ReactElement => {
   });
 
   return (
-    <Fragment>
-      <div className="h-[15rem] w-full flex items-end justify-center font-bold text-[1.8rem] md:h-[17rem] lg:h-[10rem]">
-        <h1 className="text-center w-[80vw]">Pendaftaran Mahasiswa Baru</h1>
-      </div>
-      <form
-        onSubmit={onSubmit}
-        className="flex justify-center gap-5 mt-[2rem] md:mt-[3rem]"
-      >
-        <div className="w-[80vw] md:w-[60vw] lg:w-[40vw] xl:w-[30vw]">
-          <TextField
-            name="nik"
-            variant="md"
-            control={control}
-            label="NIK"
-            placeholder="Masukan NIK"
-          />
-          <TextField
-            name="fullname"
-            variant="md"
-            control={control}
-            label="Nama Lengkap"
-            placeholder="Masukan nama lengkap"
-          />
-          <TextField
-            name="email"
-            variant="md"
-            control={control}
-            label="Email"
-            placeholder="Masukan email"
-          />
-          <TextField
-            name="password"
-            type="password"
-            variant="md"
-            control={control}
-            label="Password"
-            placeholder="Masukan password"
-          />
-          <div className="lg:mt-2 flex justify-start">
-            <CheckBox
-              name="aggrement"
-              control={control}
-              size="lg"
-              label="Dengan mendaftar saya menyetujui syarat dan ketentuan yang berlaku"
+    <section className="w-full h-screen  bg-primary-green flex justify-center">
+      <div className="w-[52.5rem] h-[30.625rem] bg-grayscale-1 rounded-xl overflow-hidden flex font-bebasNeue ">
+        <figure
+          className="w-[15.75rem] bg-cover bg-right object-center bg- bg-no-repeat bg-blend-multiply bg-grayscale-5"
+          style={{
+            backgroundImage: ` url("../foto-mahasiswa-bareng-2.jpg")`,
+          }}
+        >
+          <div className="w-full h-full flex justify-center items-center gap-x-4 ">
+            <Image src={neoLogo} alt="neo-uninus" className="w-[4.375rem] " />
+            <div className="border-r-[1px] border-r-primary-white h-24"></div>
+            <Image
+              src={hybridLogo}
+              alt="hybrid-uninus"
+              className="w-[6.25rem]"
             />
           </div>
-          <div className="flex justify-center mt-[1.5rem]">
-            <Button type="submit" loading={isLoading} size="sm" width="w-full">
-              Daftar
-            </Button>
+        </figure>
+        <div className="w-[36.75rem] h-[30.625rem] p-12">
+          <div className="w-full h-16 font-bold text-3xl">
+            <h1 className="tracking-tight">REGISTRASI</h1>
+            <div className="w-32 rounded-sm bg-primary-green h-1 mt-1"></div>
           </div>
-          <div className="flex justify-center mt-[1rem] mb-[1rem]">
-            <p>
-              Sudah memiliki akun?{' '}
-              <Link
-                href={'/login'}
-                className="underline text-bold cursor-pointer"
+          <form
+            onSubmit={onSubmit}
+            className="w-full flex flex-col h-2/3 relative "
+          >
+            <div className="grid grid-cols-2 gap-x-4 ">
+              <div className="flex flex-col w-full gap-x-5 gap-y-4">
+                <TextField
+                  name="fullname"
+                  variant="sm"
+                  control={control}
+                  label="Nama Lengkap"
+                  placeholder="Masukan nama lengkap"
+                />
+                <TextField
+                  name="email"
+                  variant="sm"
+                  type="email"
+                  control={control}
+                  label="Email Aktif"
+                  placeholder="Masukan Email Aktif"
+                />
+              </div>
+
+              <div className="flex flex-col w-full gap-x-5 gap-y-4">
+                <TextField
+                  name="nik"
+                  variant="sm"
+                  control={control}
+                  label="Nomor Induk Keluarga"
+                  placeholder="Masukan NIK"
+                />
+                <TextField
+                  name="password"
+                  variant="sm"
+                  control={control}
+                  label="Password"
+                  type="password"
+                  placeholder="Masukkan Password"
+                />
+              </div>
+            </div>
+            <div className="mt-6">
+              <p className="text-sm mb-2 text-grayscale-4">
+                Sudah memiliki akun?{' '}
+                <Link
+                  href={'/login'}
+                  className="text-secondary-green-1 font-bold ml-1"
+                >
+                  Login
+                </Link>
+              </p>
+              <Button
+                type="submit"
+                loading={isLoading}
+                size="sm"
+                width="w-full"
               >
-                Masuk
-              </Link>
-            </p>
-          </div>
+                Lakukan Registrasi
+              </Button>
+            </div>
+          </form>
         </div>
-      </form>
-    </Fragment>
+      </div>
+    </section>
   );
 };
 
