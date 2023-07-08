a7dfeb6525ab83b3074b3895b262bdb90e02741a
Golfantara <122778300+Golfantara@users.noreply.github.com>
2023-07-08 12:18:33 +0700

    Improvement/apidocs auth dan user (#190)
    * improvement:reset-password
    
    * improvement:apidocs-auth-student
    
    * improvement:apidocs-auth-users

--- libs/entities/src/dto/auth-dto/index.ts
+++ libs/entities/src/dto/auth-dto/index.ts
@@ -4,4 +4,6 @@ export * from './logout-dto';
 export * from './register-dto';
 export * from './req-user.dto';
 export * from './reset-password-dto';
+export * from './forgot-password-dto';
 export * from './verify-dto';
+export * from './logout-dto';
