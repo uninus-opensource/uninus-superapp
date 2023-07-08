ce12599642633792216ade69f07ddb575afdd644
EL5225 <luthfiramadhan155@gmail.com>
2023-07-08 07:19:03 +0700

    improvement: navbar functional sidebar

--- libs/components/src/molecules/sidebar/index.tsx
+++ libs/components/src/molecules/sidebar/index.tsx
@@ -10,12 +10,12 @@ export const Sidebar: FC<TSidebarProps> = ({
 }): ReactElement => {
   return (
     <div
-      className={`flex justify-end w-full bg-gray-800 bg-opacity-40 h-full fixed z-50  ${
+      className={`flex justify-end w-full bg-grayscale-8 bg-opacity-40 h-full fixed z-50  ${
         showSidebar ? 'translate-x-0' : 'translate-x-full'
       } lg:hidden`}
     >
       <section
-        className={`w-[70vw] h-full bg-green-930 fixed z-50 delay-100 transition-transform py-7 px-5  ${
+        className={`w-5/6 h-full bg-primary-green fixed z-50 delay-100 transition-transform py-7 px-5  ${
           showSidebar ? 'translate-x-0' : 'translate-x-full'
         }`}
       >
