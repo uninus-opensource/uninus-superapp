8488b0bdd2c6f4af8304d8c16b61d8a799da7d7a
Fenny Oktaviani <fnny04@gmail.com>
2023-07-08 21:22:37 +0700

    feature : slicing dashboard pembayaran

--- apps/pmb/app/dashboard/pendaftaran/page.tsx
+++ apps/pmb/app/dashboard/pendaftaran/page.tsx
@@ -1,8 +1,52 @@
+'use client';
 import { NextPage } from 'next';
 import { ReactElement } from 'react';
+import DashboardLayout from '../../layouts/DashboardLayout';
+import { Button, SelectField } from '@uninus/components';
 
 const DashboardPendaftaran: NextPage = (): ReactElement => {
-  return <main></main>;
+  return (
+    <DashboardLayout>
+      <h1 className="text-4xl font-bold pb-8">Pendaftaran</h1>
+      <section className="w-full h-auto bg-primary-white p-8">
+        <div className="flex flex-col gap-8">
+          <SelectField
+            name="program"
+            label="Program"
+            size="md"
+            placeholder="Pilih Program Pendidikan"
+            status="none"
+            options={[
+              'Program Sarjana(S1) 2023/2024',
+              'Program Pascasarjana(S2) 2023/2024',
+            ]}
+            value="Pilihan Program"
+          />
+          <SelectField
+            name="seleksi"
+            label="Jalur Seleksi"
+            size="md"
+            placeholder="Pilih Jalur Seleksi"
+            status="none"
+            options={['Beasiswa KIP', 'Mandiri']}
+            value="Jalur Seleksi"
+          />
+          <SelectField
+            name="pembayaran"
+            label="Metode Pembayaran"
+            size="md"
+            placeholder="Pilih Metode Pembayaran"
+            status="none"
+            options={['Transfer Antar Bank', 'Datang Langsung']}
+            value="Metode Pembayaran"
+          />
+          <Button variant="elevated" size="sm" width="w-48" height="h-12">
+            Daftar Sekarang
+          </Button>
+        </div>
+      </section>
+    </DashboardLayout>
+  );
 };
 
 export default DashboardPendaftaran;
