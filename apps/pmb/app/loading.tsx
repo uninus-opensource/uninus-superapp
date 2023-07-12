7c24a8680276205e99b21a4319dc3e0d9401a932
Fenny Oktaviani <fnny04@gmail.com>
2023-07-12 07:13:02 +0700

    impovement : add loading spinner

--- apps/pmb/app/loading.tsx
+++ apps/pmb/app/loading.tsx
@@ -3,8 +3,8 @@ import { LoadingSpinner } from '@uninus/components';
 
 export default function Loading() {
   return (
-    <div className="flex bg-slate-2 h-screen w-screen justify-center items-center">
+    <section className="flex bg-slate-2 h-screen w-screen justify-center items-center">
       <LoadingSpinner />
-    </div>
+    </section>
   );
 }
