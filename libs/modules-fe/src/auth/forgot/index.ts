89ce8a839aaca5caf91a6899db23f9679507a765
Rohendo <108710826+RohendoJ@users.noreply.github.com>
2023-07-13 18:31:36 +0700

    improvement: integrasi benerin hook forgot password (#278)
    * improvement: benerin hook forgot password
    
    * improve: forgot integrasi zod

--- libs/modules-fe/src/auth/forgot/index.ts
+++ libs/modules-fe/src/auth/forgot/index.ts
@@ -1,1 +1,4 @@
 export * from './module';
+export * from './api';
+export * from './hook';
+export * from './schema';
--- libs/modules-fe/src/auth/forgot/module.tsx
+++ libs/modules-fe/src/auth/forgot/module.tsx
@@ -2,17 +2,40 @@
 import { Button, TextField } from '@uninus/components';
 import { FC, ReactElement } from 'react';
 import { useForm } from 'react-hook-form';
+import { useForgot } from './hook';
+import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { TVSForgot, VSForgot } from './schema';
 
 export const ForgotModule: FC = (): ReactElement => {
-  const { control, handleSubmit } = useForm({
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<TVSForgot>({
+    mode: 'all',
+    resolver: zodResolver(VSForgot),
     defaultValues: {
       email: '',
-      password: '',
-      aggreement: false,
     },
   });
+
+  const { mutate: forgot } = useForgot();
+  const router = useRouter();
+
+  const onSubmit = handleSubmit((data) => {
+    try {
+      forgot(data);
+      router.push('/auth/verif-email');
+    } catch (error) {
+      console.log(error);
+    }
+  });
   return (
-    <form className="w-full h-full p-12 lg:px-12 lg:py-4 flex flex-col justify-center items-center">
+    <form
+      className="w-full h-full p-12 lg:px-12 lg:py-4 flex flex-col justify-center items-center"
+      onSubmit={onSubmit}
+    >
       <div className="w-full flex flex-col gap-6">
         <h1 className="text-2xl font-bold text-primary-black font-bebasNeue w-50%">
           LUPA PASSWORD ?
@@ -27,8 +50,10 @@ export const ForgotModule: FC = (): ReactElement => {
           placeholder="Masukan email"
           control={control}
           required
+          status={errors?.email ? 'error' : undefined}
+          message={errors?.email?.message}
         />
-        <Button>Lanjutkan</Button>
+        <Button disabled={!isValid}>Lanjutkan</Button>
       </div>
     </form>
   );
