042bf1b25646d002c72965c3445c315e5d991fd5
EL5225 <luthfiramadhan155@gmail.com>
2023-07-15 16:14:47 +0700

    bugfix: error build

--- libs/components/src/molecules/inputs/select/types.ts
+++ libs/components/src/molecules/inputs/select/types.ts
@@ -4,6 +4,7 @@ import { FieldValues, UseControllerProps } from 'react-hook-form';
 export type TSelectProps<T extends FieldValues> = UseControllerProps<T> & {
   label: string;
   width?: string;
+  value?: string;
   size: 'sm' | 'md';
   name: string;
   options: string[];
