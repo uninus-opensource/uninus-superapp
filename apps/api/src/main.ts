b81a05d22f8ffe13a5e1e9b1d4013345c97b04f7
sugiartofahmi <sugiartofahmi@gmail.com>
2023-07-16 04:31:22 +0700

    improvement: add swagger ApiBearerAuth

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -24,6 +24,7 @@ async function bootstrap() {
     .setTitle('UNINUS API')
     .setDescription('API description')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
