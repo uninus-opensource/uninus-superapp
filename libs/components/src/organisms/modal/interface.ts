a371118055255c2ae157ec7cf8b1a46a26b9185e
Luthfi Ahmad Fauzi <110830417+luthfiahmdf@users.noreply.github.com>
2023-07-20 20:50:32 +0700

    Improvement/modal on logout (#393)
    * feature: modal-onLogout
    
    * feature: modal-onLogout
    
    * feature: modal-onLogout
    
    * feature: adding responsive on modal Logout
    
    * feature: adding responsive on modal Logout
    
    * improvement: ubah desain modal

--- libs/components/src/organisms/modal/interface.ts
+++ libs/components/src/organisms/modal/interface.ts
@@ -3,7 +3,7 @@ import { ReactNode } from 'react';
 export interface ModalProps {
   showModal: boolean;
   onClose: () => void;
-
+  iconClose?: boolean;
   modalTitle: string;
   children?: ReactNode;
   submitText?: string;
