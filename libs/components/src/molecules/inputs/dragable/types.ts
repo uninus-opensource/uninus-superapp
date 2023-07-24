c4d7f8a8d21728912657d244fd38f0c8fe2b1050
Fenny Oktaviani <fnny04@gmail.com>
2023-07-24 18:49:35 +0700

    improvement : fix type

--- libs/components/src/molecules/inputs/dragable/types.ts
+++ libs/components/src/molecules/inputs/dragable/types.ts
@@ -1,6 +1,6 @@
 import { FieldValues, UseControllerProps } from 'react-hook-form';
 
-export type TUploadFieldProps<T> = UseControllerProps<FieldValues, string> & {
+export type TUploadFieldProps = UseControllerProps<FieldValues, string> & {
   className?: string;
   labels?: string;
   required?: boolean;
