9bd9e4271c77d336cf8477ed909dcd70453ca5e5
Fenny Oktaviani <79123882+fnny04@users.noreply.github.com>
2023-07-25 02:02:38 +0700

    feature : add metadata (#436)

--- apps/pmb/app/auth/reset-password/page.tsx
+++ apps/pmb/app/auth/reset-password/page.tsx
@@ -1,6 +1,11 @@
 import { ResetModule } from '@uninus/modules-fe';
 import { NextPage } from 'next';
 import { ReactElement } from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'PMB | Ubah Password',
+};
 
 const resetPassword: NextPage = (): ReactElement => <ResetModule />;
 export default resetPassword;
--- apps/pmb/app/beasiswa/page.tsx
+++ apps/pmb/app/beasiswa/page.tsx
@@ -1,6 +1,11 @@
 import { ReactElement } from 'react';
 import { NextPage } from 'next';
 import { ModulBeasiswa } from '@uninus/modules-fe';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'PMB | Beasiswa',
+};
 
 const BeasiswaPage: NextPage = (): ReactElement => <ModulBeasiswa />;
 export default BeasiswaPage;
