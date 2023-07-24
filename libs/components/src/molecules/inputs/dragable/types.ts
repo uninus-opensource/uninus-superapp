28ec47d1049aaaac4dc158e54d35dc12b6b8b205
Fenny Oktaviani <fnny04@gmail.com>
2023-07-24 18:49:06 +0700

    improvement : active button disabled

--- libs/components/src/molecules/inputs/dragable/types.ts
+++ libs/components/src/molecules/inputs/dragable/types.ts
@@ -3,4 +3,5 @@ import { FieldValues, UseControllerProps } from 'react-hook-form';
 export type TUploadFieldProps<T> = UseControllerProps<FieldValues, string> & {
   className?: string;
   labels?: string;
+  required?: boolean;
 };
