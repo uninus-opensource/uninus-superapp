2627eabe1384c3d85b9de263473862d4732c7aee
sugiartofahmi <sugiartofahmi@gmail.com>
2023-07-25 20:21:58 +0700

    improvement: add location schema

--- libs/models/src/prisma/migrations/20230724045341_/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Students" ADD COLUMN     "academic_year" TEXT,
-ADD COLUMN     "program" TEXT,
-ADD COLUMN     "registration_wave" TEXT,
-ADD COLUMN     "selection_type" TEXT;
--- libs/models/src/prisma/migrations/20230724091109_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Students" ADD COLUMN     "phone_number" TEXT;
