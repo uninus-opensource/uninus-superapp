996949e433822b7bcc706df8c5ac7dd25e3d0380
RohendoJ <rohendoj3134.github@gmail.com>
2023-07-26 12:48:04 +0700

    remove console

--- libs/modules-fe/src/auth/login/next-auth.ts
+++ libs/modules-fe/src/auth/login/next-auth.ts
@@ -21,7 +21,6 @@ export const authOptions: NextAuthOptions = {
           return data;
         } catch (err) {
           const error = err as TMetaErrorResponse;
-          console.log(error);
           if (error?.response?.status === 422) {
             throw new Error(error.response.data.message);
           }
