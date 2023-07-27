ea21688602fda82487ad6b1904ba79de646873ae
YusronF <103730280+YusronFauzanN@users.noreply.github.com>
2023-07-27 17:09:25 +0700

    feature: add crud endpoint for location (#470)
    * feature: add crud endpoint for location
    
    * bugfix: Fix error link unused variabel
    
    * feature: remove endpoint crud
    
    * feature: remove parseInt

---
+++ libs/services/src/location/index.ts
@@ -0,0 +1,33 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from '@uninus/models';
+
+@Injectable()
+export class LocationService {
+  constructor(private prisma: PrismaService) {}
+
+  async getLocation(province: string, city: string) {
+    const queryOptions: any = {};
+
+    if (province) {
+      queryOptions.where = { id: parseInt(province) };
+      queryOptions.include = { cities: {} };
+    }
+
+    if (city) {
+      queryOptions.include.cities.where = { id: parseInt(city) };
+      queryOptions.include.cities.include = { sub_district: {} };
+    }
+
+    const location = await this.prisma.province.findMany(queryOptions);
+
+    if (!location) {
+      throw new BadRequestException('Data tidak ditemukan', {
+        cause: new Error(),
+      });
+    }
+
+    return {
+      province: location,
+    };
+  }
+}
