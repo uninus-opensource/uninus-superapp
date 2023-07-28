603d45be7cf4992daf2aaafecd7a9423277c62a8
Fenny Oktaviani <79123882+fnny04@users.noreply.github.com>
2023-07-28 08:27:27 +0700

    Improvement : add feature search on tuition fee page  (#477)
    * improvement : add feature search on tuition fee
    
    * fix : fix wording exclude tuition fee
    
    * improve : responsive

--- libs/components/src/molecules/inputs/search/ic-search.tsx
+++ libs/components/src/molecules/inputs/search/ic-search.tsx
@@ -3,17 +3,19 @@ import { FC, ReactElement } from 'react';
 const IconSearch: FC = (): ReactElement => {
   return (
     <svg
+      className="w-4 h-4 text-gray-500 "
       aria-hidden="true"
-      className="w-5 h-5 text-gray-500 dark:text-gray-400"
-      fill="currentColor"
-      viewBox="0 0 20 20"
       xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 20 20"
     >
       <path
-        fillRule="evenodd"
-        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-        clipRule="evenodd"
-      ></path>
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+      />
     </svg>
   );
 };
