13e6a65249cc0843ebeee400623b05a1a1b3e0b8
sugiartofahmi <sugiartofahmi@gmail.com>
2023-07-28 22:42:01 +0700

    bugfix: fix eslint error

--- libs/utilities/src/exclude-schema/index.ts
+++ libs/utilities/src/exclude-schema/index.ts
@@ -1,4 +1,4 @@
-export const excludeSchema = (data: any, keys: String[]) => {
+export const excludeSchema = (data: any, keys: string[]) => {
   return Object.fromEntries(
     Object.entries(data).filter(([key]) => !keys.includes(key))
   );
