7ed2bd9a98b7b1969fffa3c12c30aac006579823
RohendoJ <rohendoj3134.github@gmail.com>
2023-07-31 18:14:13 +0700

    improvement: formatting phone number register from UI UX

--- libs/web/modules/src/auth/register/module.tsx
+++ libs/web/modules/src/auth/register/module.tsx
@@ -46,7 +46,7 @@ export const RegisterModule: FC = (): ReactElement => {
     <form
       key="auth-register"
       onSubmit={onSubmit}
-      className="w-full h-auto px-5 lg:px-12 py-5 flex flex-col gap-y-6"
+      className="w-full h-auto px-5 lg:px-12 py-5 flex flex-col gap-y-3 md:gap-y-1"
     >
       <div className="w-full justify-start flex">
         <div className="flex flex-col justify-end lg:gap-y-1 py-2">
@@ -58,52 +58,73 @@ export const RegisterModule: FC = (): ReactElement => {
       </div>
       <div className="h-auto w-full">
         <div className="grid gap-x-4 w-full grid-cols-1 md:grid-cols-2 grid-rows-2">
-          <TextField
-            name="fullname"
-            type="text"
-            variant="md"
-            label="Nama Lengkap"
-            placeholder="Masukan Nama Lengkap"
-            control={control}
-            required
-            status={errors?.fullname ? 'error' : undefined}
-            message={errors?.fullname?.message}
-          />
-          <TextField
-            name="phone_number"
-            type="text"
-            variant="md"
-            label="Nomor Handphone"
-            placeholder="Masukan Nomor Handphone"
-            control={control}
-            required
-            status={errors?.phone_number ? 'error' : undefined}
-            message={errors?.phone_number?.message}
-            maxlenght={16}
-            inputMode="tel"
-          />
-          <TextField
-            name="email"
-            type="email"
-            variant="md"
-            label="Email"
-            placeholder="Masukan email"
-            control={control}
-            required
-            status={errors?.email ? 'error' : undefined}
-            message={errors?.email?.message}
-          />
-          <TextField
-            name="password"
-            type="password"
-            variant="md"
-            label="Kata Sandi"
-            control={control}
-            placeholder="Masukan Kata Sandi"
-            required
-            status={errors?.password ? 'error' : undefined}
-            message={errors?.password?.message}
-          />
+          <div className="h-24">
+            <TextField
+              name="fullname"
+              type="text"
+              variant="md"
+              label="Nama Lengkap"
+              placeholder="Masukan Nama Lengkap"
+              control={control}
+              required
+              status={errors?.fullname ? 'error' : undefined}
+              message={errors?.fullname?.message}
+            />
+          </div>
+          <div className="h-24 flex flex-col mt-1">
+            <label className="text-xs md:text-sm xl:text-md lg:text-xs 2xl:text-lg font-semibold">
+              Nomor Handphone <span className="text-red-4">*</span>
+            </label>
+
+            <div className="flex mt-1">
+              <div>
+                <div className="bg-primary-green grid place-items-center rounded-md px-[0.7rem] py-[0.6rem] mt-1 lg:py-[0.85rem]">
+                  <p className="text-primary-white text-[12px]">+62</p>
+                </div>
+              </div>
+
+              <div className="w-full h-16">
+                <TextField
+                  name="phone_number"
+                  type="number"
+                  variant="md"
+                  placeholder="Masukan Nomor Handphone"
+                  control={control}
+                  required
+                  status={errors?.phone_number ? 'error' : undefined}
+                  message={errors?.phone_number?.message}
+                  maxlenght={16}
+                  inputMode="tel"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="h-24">
+            <TextField
+              name="email"
+              type="email"
+              variant="md"
+              label="Email"
+              placeholder="Masukan email"
+              control={control}
+              required
+              status={errors?.email ? 'error' : undefined}
+              message={errors?.email?.message}
+            />
+          </div>
+          <div className="h-24">
+            <TextField
+              name="password"
+              type="password"
+              variant="md"
+              label="Kata Sandi"
+              control={control}
+              placeholder="Masukan Kata Sandi"
+              required
+              status={errors?.password ? 'error' : undefined}
+              message={errors?.password?.message}
+            />
+          </div>
         </div>
       </div>
       <div className="h-[33%] md:h-[36%] lg:h-[31%] flex flex-col gap-3 lg:mt-[0.5rem]">
