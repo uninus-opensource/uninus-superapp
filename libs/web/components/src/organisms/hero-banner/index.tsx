095c83fdac0387bcb6255076ae735cba74de7f72
Muhammad Luthfi Ramadhan <108562600+EL5225@users.noreply.github.com>
2023-08-02 20:34:47 +0700

    improvement: slider hero landing (#573)
    * improvement: slider hero landing
    
    * fixbug

--- libs/web/components/src/organisms/hero-banner/index.tsx
+++ libs/web/components/src/organisms/hero-banner/index.tsx
@@ -1,8 +1,9 @@
-import { FC, ReactElement } from 'react';
+import { FC, ReactElement, useState } from 'react';
 import { TSliderProps, TbannerProps } from './type';
 import { Reveal, Button } from '../../atoms';
 import Image from 'next/image';
-import AliceCarousel from 'react-alice-carousel';
+import AliceCarousel, { EventObject } from 'react-alice-carousel';
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 
 export const HeroBanner: FC<TbannerProps> = ({
   heroTitle,
@@ -16,11 +17,36 @@ export const HeroBanner: FC<TbannerProps> = ({
   blur = false,
   isSlider = false,
 }): ReactElement => {
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+
+  const slidePrev = () => {
+    if (activeIndex <= 0) {
+      setActiveIndex(heroSlider.length - 1);
+    } else {
+      setActiveIndex(activeIndex - 1);
+    }
+  };
+  const slideNext = () => {
+    if (activeIndex >= heroSlider.length - 1) {
+      setActiveIndex(0);
+    } else {
+      setActiveIndex(activeIndex + 1);
+    }
+  };
+
+  const sync = (e: EventObject) => {
+    if (e.item > 3) {
+      setActiveIndex(e.item * 0);
+    } else {
+      setActiveIndex(e.item);
+    }
+  };
+
   const heroSlider: TSliderProps[] = [
     {
       sliderSection: (
         <header
-          className={`bg-center ${backgrounColor} flex justify-center items-center relative bg-fixed object-center bg-cover lg:w-full h-auto bg-no-repeat bg-blend-overlay`}
+          className={`bg-center ${backgrounColor} flex justify-center items-center bg-fixed object-center bg-cover lg:w-full xl:h-screen lg:h-[120vh] h-[50vh] relative bg-no-repeat bg-blend-overlay`}
           style={{
             backgroundImage: `url(${heroImages})`,
           }}
@@ -28,17 +54,17 @@ export const HeroBanner: FC<TbannerProps> = ({
           <section
             className={`px-4 mx-auto w-full h-full ${
               blur ? 'backdrop-blur-sm' : ''
-            } text-center md:pt-20 md:pb-16 lg:pt-40`}
+            } text-center md:pt-16 lg:pt-28 xl:pt-16`}
           >
             <Reveal w="w-full" blur={blur}>
-              <div className="flex items-center flex-col w-full">
-                <div className="text-base sm:text-xl md:text-4xl xl:text-5xl py-2 lg:py-4 font-black text-primary-white leading-normal uppercase lg:pt-10 pt-32">
+              <div className="flex items-center justify-center lg:h-screen flex-col w-full">
+                <div className="text-sm sm:text-xl md:text-4xl xl:text-5xl py-2 lg:py-4 font-black text-primary-white leading-normal uppercase pt-32">
                   {subTitle}
                 </div>
-                <h2 className="mb-5 lg:mb-8 text-lg md:text-3xl xl:text-4xl font-semibold text-primary-white">
+                <h2 className="mb-5 lg:mb-8 text-base md:text-3xl xl:text-4xl font-semibold text-primary-white">
                   {heroTitle}
                 </h2>
-                <h1 className="mb-1 lg:mb-4 text-xl sm:text-xl font-bold md:font-medium lg:font-bold md:text-3xl lg:text-5xl text-primary-white relative bottom-4">
+                <h1 className="lg:mb-4 text-lg sm:text-xl font-bold md:font-medium lg:font-bold md:text-3xl lg:text-5xl text-primary-white relative bottom-4">
                   {heroTitle2}
                 </h1>
                 <p className="text-lg md:text-3xl text-primary-white font-bold">
@@ -53,7 +79,7 @@ export const HeroBanner: FC<TbannerProps> = ({
                   ></div>
                 </div>
                 {isDownload ? (
-                  <section className="flex my-5 lg:mt-12 gap-8 ">
+                  <section className="flex mt-4 lg:mt-12 gap-8 ">
                     <Button
                       href="https://pmb.uninus.ac.id/wp-content/uploads/2023/03/Brosur-Program-Sarjana.pdf"
                       variant="outlined"
@@ -86,7 +112,7 @@ export const HeroBanner: FC<TbannerProps> = ({
           quality={100}
           width={1000}
           height={500}
-          className={`bg-center flex justify-center items-center absolute top-28 mt-2 lg:h-auto lg:w-full`}
+          className={`bg-center flex justify-center items-center absolute mt-24 lg:mt-2 lg:h-auto lg:w-full`}
         />
       ),
     },
@@ -98,7 +124,7 @@ export const HeroBanner: FC<TbannerProps> = ({
           quality={100}
           width={1000}
           height={500}
-          className={`bg-center flex justify-center items-center absolute top-28 mt-2 lg:h-auto lg:w-full`}
+          className={`bg-center flex justify-center items-center absolute mt-24 lg:mt-2 lg:h-auto lg:w-full`}
         />
       ),
     },
@@ -110,7 +136,7 @@ export const HeroBanner: FC<TbannerProps> = ({
           quality={100}
           width={1000}
           height={500}
-          className={`bg-center flex justify-center items-center absolute top-28 mt-2 lg:h-auto lg:w-full`}
+          className={`bg-center flex justify-center items-center absolute mt-24 lg:mt-2 lg:h-auto lg:w-full`}
         />
       ),
     },
@@ -118,17 +144,34 @@ export const HeroBanner: FC<TbannerProps> = ({
 
   const cardProps = {
     items: heroSlider.map((item) => item.sliderSection),
+
+    animationDuration: 1000,
     autoPlay: true,
     autoPlayInterval: 4000,
-    animationDuration: 1000,
     infinite: true,
+    activeIndex: activeIndex,
     disableButtonsControls: true,
     disableDotsControls: true,
+    touchTracking: true,
     mouseTracking: true,
   };
 
   return isSlider ? (
-    <AliceCarousel {...cardProps} />
+    <div className="flex items-center justify-center">
+      <AliceCarousel {...cardProps} onSlideChanged={sync} autoHeight />
+      <button
+        className="hidden lg:block absolute z-30 left-3 text-5xl text-primary-white lg:mt-16"
+        onClick={slidePrev}
+      >
+        <LeftOutlined />
+      </button>
+      <button
+        className="hidden lg:block absolute z-30 right-3 text-5xl text-primary-white lg:mt-20"
+        onClick={slideNext}
+      >
+        <RightOutlined />
+      </button>
+    </div>
   ) : (
     <header
       className={`bg-center ${backgrounColor} flex justify-center items-center relative bg-fixed object-center bg-cover lg:w-full h-auto bg-no-repeat bg-blend-overlay`}
