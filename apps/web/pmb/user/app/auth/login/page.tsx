9f758faca47994e4e652b9e2609e9ac6623ed1d5
Maulana Sodiqin <53475078+maulanasdqn@users.noreply.github.com>
2023-08-04 22:21:43 +0700

    feature: pmb admin login (#626)
    Co-authored-by: Maulana Sodiqin <maulansdqn@gmail.com>

--- apps/web/pmb/user/app/auth/login/page.tsx
+++ apps/web/pmb/user/app/auth/login/page.tsx
@@ -1,11 +1,11 @@
 import { NextPage } from "next";
 import { ReactElement } from "react";
-import { LoginModule } from "@uninus/web/modules";
+import { LoginUserModule } from "@uninus/web/modules";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "PMB | Masuk",
 };
 
-const LoginPage: NextPage = (): ReactElement => <LoginModule key={"login"} />;
+const LoginPage: NextPage = (): ReactElement => <LoginUserModule key={"login"} />;
 export default LoginPage;
