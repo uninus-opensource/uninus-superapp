289a44d243963ecd4337be0e097dd604b15f574e
Muhammad Luthfi Ramadhan <108562600+EL5225@users.noreply.github.com>
2023-08-07 16:45:00 +0700

    feature: add new navbar and bugfix slider sliding (#652)

--- libs/web/components/src/organisms/hero-banner/index.tsx
+++ libs/web/components/src/organisms/hero-banner/index.tsx
@@ -1,8 +1,8 @@
-import { FC, ReactElement, useState } from "react";
+import { FC, ReactElement, useRef } from "react";
 import { TSliderProps, TbannerProps } from "./type";
 import { Reveal, Button } from "../../atoms";
 import Image from "next/image";
-import AliceCarousel, { EventObject } from "react-alice-carousel";
+import AliceCarousel from "react-alice-carousel";
 import { LeftOutlined, RightOutlined } from "@ant-design/icons";
 
 export const HeroBanner: FC<TbannerProps> = ({
@@ -17,36 +17,13 @@ export const HeroBanner: FC<TbannerProps> = ({
   blur = false,
   isSlider = false,
 }): ReactElement => {
-  const [activeIndex, setActiveIndex] = useState<number>(0);
-
-  const slidePrev = () => {
-    if (activeIndex <= 0) {
-      setActiveIndex(heroSlider.length - 1);
-    } else {
-      setActiveIndex(activeIndex - 1);
-    }
-  };
-  const slideNext = () => {
-    if (activeIndex >= heroSlider.length - 1) {
-      setActiveIndex(0);
-    } else {
-      setActiveIndex(activeIndex + 1);
-    }
-  };
-
-  const sync = (e: EventObject) => {
-    if (e.item > heroSlider.length - 1) {
-      setActiveIndex(e.item * 0);
-    } else {
-      setActiveIndex(e.item);
-    }
-  };
+  const carousel = useRef<AliceCarousel>(null);
 
   const heroSlider: TSliderProps[] = [
     {
       sliderSection: (
         <header
-          className={`bg-center ${backgrounColor} flex justify-center items-center bg-fixed bg-center bg-cover lg:w-full xl:h-screen lg:h-[120vh] h-auto pb-6 lg:pb-0 relative w-screen bg-no-repeat bg-blend-overlay`}
+          className={`bg-center ${backgrounColor} flex justify-center items-center bg-fixed bg-cover lg:w-full xl:h-screen h-auto relative w-screen bg-no-repeat bg-blend-overlay`}
           style={{
             backgroundImage: `url(${heroImages})`,
           }}
@@ -54,10 +31,10 @@ export const HeroBanner: FC<TbannerProps> = ({
           <section
             className={`px-4 mx-auto w-full h-full ${
               blur ? "backdrop-blur-sm" : ""
-            } text-center md:pt-16 lg:pt-28 xl:pt-16`}
+            } text-center pb-6 md:pb-0 lg:pt-28 xl:pt-16`}
           >
             <Reveal w="w-full" blur={blur}>
-              <div className="flex items-center justify-center lg:h-screen flex-col w-full">
+              <div className="flex items-center justify-center md:h-screen flex-col w-full">
                 <div className="text-sm sm:text-xl md:text-4xl xl:text-5xl py-2 lg:py-4 font-black text-primary-white leading-normal uppercase pt-32">
                   {subTitle}
                 </div>
@@ -151,10 +128,9 @@ export const HeroBanner: FC<TbannerProps> = ({
     items: heroSlider.map((item) => item.sliderSection),
 
     animationDuration: 1000,
-    autoPlay: true,
+    // autoPlay: true,
     autoPlayInterval: 5000,
     infinite: true,
-    activeIndex: activeIndex,
     disableButtonsControls: true,
     disableDotsControls: true,
     touchTracking: true,
@@ -163,16 +139,22 @@ export const HeroBanner: FC<TbannerProps> = ({
 
   return isSlider ? (
     <div className="flex items-center justify-center">
-      <AliceCarousel {...cardProps} onSlideChanged={sync} autoHeight />
+      <AliceCarousel
+        key="carousel"
+        {...cardProps}
+        autoHeight
+        animationType="slide"
+        ref={carousel}
+      />
       <button
         className="hidden lg:block absolute z-30 left-3 text-5xl text-primary-white lg:mt-16"
-        onClick={slidePrev}
+        onClick={(e) => carousel?.current?.slidePrev(e)}
       >
         <LeftOutlined />
       </button>
       <button
         className="hidden lg:block absolute z-30 right-3 text-5xl text-primary-white lg:mt-20"
-        onClick={slideNext}
+        onClick={(e) => carousel?.current?.slideNext(e)}
       >
         <RightOutlined />
       </button>
