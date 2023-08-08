be974f96ff7f200311da567e35bdc7b7acb8fb12
Luthfi Ahmad Fauzi <110830417+luthfiahmdf@users.noreply.github.com>
2023-08-08 17:51:30 +0700

    Bugfix/landing page biaya kuliah (#673)
    * feature:program-page
    
    * bugfix:landing-page-biaya-kuliah

---
+++ libs/web/components/src/molecules/inputs/radio/enum.ts
@@ -0,0 +1,16 @@
+export enum RADIO_SIZE {
+  SM = "sm",
+  MD = "md",
+  LG = "lg",
+}
+
+export enum RADIO_VARIANT {
+  PRIMARY = "primary",
+  WARNING = "warning",
+}
+
+export enum LABEL_SIZE {
+  SM = "sm",
+  MD = "md",
+  LG = "lg",
+}
