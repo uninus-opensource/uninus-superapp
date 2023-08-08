1d69b908058dba3793b7f00831c14c4241bfcdf7
rafli <rafliforbusiness@gmail.com>
2023-08-09 01:18:57 +0700

    Mochamad Rafli | improvement upload file service

--- libs/api/controllers/src/file/index.spec.ts
+++ libs/api/controllers/src/file/index.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { FileController } from '../file/';
+import { FileController } from './index';
+import { FileService } from '@uninus/api/services';
+import { ConfigService } from '@nestjs/config';
 
 describe('FileController', () => {
   let controller: FileController;
@@ -7,6 +9,24 @@ describe('FileController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FileController],
+      providers: [FileService, {
+        provide: ConfigService,
+        useValue:{
+          getOrThrow: jest.fn((key:string)=>{
+            if(key=="BUCKET"){
+              return "bucket-test"
+            }else if(key=="REGION"){
+              return "region"
+            }else if(key=="AWS_ACCESS_SECRET_KEY"){
+              return "secret_key"
+            }else if(key=="AWS_ACCESS_KEY"){
+              return "access_key"
+            }
+
+            return
+          })
+        }
+      }]
     }).compile();
 
     controller = module.get<FileController>(FileController);
--- libs/api/services/src/file/index.spec.ts
+++ libs/api/services/src/file/index.spec.ts
@@ -1,12 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { FileService } from '../file';
+import { FileService } from './index';
+import { ConfigService } from '@nestjs/config';
 
 describe('FileService', () => {
   let service: FileService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FileService],
+      providers: [FileService,
+        {
+        provide: ConfigService,
+        useValue:{
+          getOrThrow: jest.fn((key:string)=>{
+            if(key=="BUCKET"){
+              return "bucket-test"
+            }else if(key=="REGION"){
+              return "region"
+            }else if(key=="AWS_ACCESS_SECRET_KEY"){
+              return "secret_key"
+            }else if(key=="AWS_ACCESS_KEY"){
+              return "access_key"
+            }
+
+            return
+          })
+        }}
+      ],
     }).compile();
 
     service = module.get<FileService>(FileService);
