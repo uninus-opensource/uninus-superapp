2b03149134b9c6c60545617e989eab5ef79ebd08
luthfiahmdf <luthfiahmdf@gmail.com>
2023-08-09 20:40:49 +0700

    feature:unit-test-search-select

--- libs/web/components/src/molecules/inputs/search/ic-search.tsx
+++ libs/web/components/src/molecules/inputs/search/ic-search.tsx
@@ -7,13 +7,14 @@ const IconSearch: FC = (): ReactElement => {
       aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
+      data-testid="search-icon"
       viewBox="0 0 20 20"
     >
       <path
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
         d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
       />
     </svg>
