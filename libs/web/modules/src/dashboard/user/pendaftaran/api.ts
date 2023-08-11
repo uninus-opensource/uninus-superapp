83f5bd241d2d05a1341d3233b6a8bd9c330ac0bc
Fenny Oktaviani <fnny04@gmail.com>
2023-08-11 22:10:14 +0700

    improvement : integration get select option form pendaftaran

---
+++ libs/web/modules/src/dashboard/user/pendaftaran/api.ts
@@ -0,0 +1,38 @@
+import { api } from "@uninus/web/services";
+import {
+  TDegreeProgramResponse,
+  IDegreeProgramRequest,
+  IDepartmentRequest,
+  TDepartmentResponse,
+  ISelectionRequest,
+  TSelectionResponse,
+} from "@uninus/entities";
+
+export const DegreeProgramGet = async (
+  params: IDegreeProgramRequest,
+): Promise<TDegreeProgramResponse> => {
+  const { data } = await api<TDegreeProgramResponse>({
+    method: "GET",
+    params,
+    url: "/degree-program",
+  });
+  return data;
+};
+
+export const DepartmentGet = async (params: IDepartmentRequest): Promise<TDepartmentResponse> => {
+  const { data } = await api<TDepartmentResponse>({
+    method: "GET",
+    params,
+    url: "/department",
+  });
+  return data;
+};
+
+export const SelectionGet = async (params: ISelectionRequest): Promise<TSelectionResponse> => {
+  const { data } = await api<TSelectionResponse>({
+    method: "GET",
+    params,
+    url: "/selection-path",
+  });
+  return data;
+};
