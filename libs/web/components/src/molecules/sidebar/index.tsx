e9ef9aaec18e55952566dfd474860700272eed67
EL5225 <luthfiramadhan155@gmail.com>
2023-08-15 09:59:43 +0700

    feature: slicing new navbar & sidebar

--- libs/web/components/src/molecules/sidebar/index.tsx
+++ libs/web/components/src/molecules/sidebar/index.tsx
@@ -1,16 +1,39 @@
 "use client";
 import { FC, ReactElement } from "react";
 import { TSidebarProps } from "./type";
+import Image from "next/image";
+import { Button, XIcon } from "../../atoms";
 
 export const Sidebar: FC<TSidebarProps> = ({ showSidebar, ...props }): ReactElement => {
   return (
-    <section
-      data-testid="sidebar-navbar"
-      className={`w-full h-full bg-primary-green text-primary-white z-40 fixed top-20 transition-transform py-7 px-5  ${
-        showSidebar ? "translate-x-0" : "translate-x-full"
-      }`}
+    <main
+      data-testid="bg-sidebar-navbar"
+      className={`${
+        showSidebar ? "-translate-x-0 lg:-translate-x-full" : "-translate-x-full"
+      } fixed w-full h-screen bg-primary-black bg-opacity-75 z-[9999]`}
     >
-      <nav className="text-primary-white">{props.children}</nav>
-    </section>
+      <section
+        data-testid="sidebar-navbar"
+        className={`w-[80vw] sm:w-[60vw] md:w-[50vw] h-full bg-primary-green text-primary-white z-40 fixed transition-transform px-5 py-6 ${
+          showSidebar ? "-translate-x-0 lg:-translate-x-full" : "-translate-x-full"
+        }`}
+      >
+        <div className="flex items-center w-full justify-between">
+          <Image
+            src={"/illustrations/Neo-Uninus.webp"}
+            width={500}
+            height={500}
+            quality={100}
+            alt="logo-uninus"
+            priority
+            className="w-44 sm:w-56 lg:w-52"
+          />
+          <Button variant="text-icon" onClick={props.closeSidebar}>
+            <XIcon />
+          </Button>
+        </div>
+        <nav className="text-primary-white text-left my-16">{props.children}</nav>
+      </section>
+    </main>
   );
 };
