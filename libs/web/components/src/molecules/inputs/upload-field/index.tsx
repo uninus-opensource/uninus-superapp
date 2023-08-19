e52743ac7e1745c7b498804fa3b1ab3fb8d12d23
dika841 <randikaa1999@gmail.com>
2023-08-20 00:49:31 +0700

    Improvement : Add variant on Global Component Upload File

--- libs/web/components/src/molecules/inputs/upload-field/index.tsx
+++ libs/web/components/src/molecules/inputs/upload-field/index.tsx
@@ -2,7 +2,7 @@
 import { FC, ReactElement } from "react";
 import { TUploadFile } from "./types";
 import { useController } from "react-hook-form";
-
+import clsx from "clsx";
 export const UploadField: FC<TUploadFile> = (props): ReactElement => {
   const {
     field: { onChange, value, ref },
@@ -10,6 +10,13 @@ export const UploadField: FC<TUploadFile> = (props): ReactElement => {
     ...props,
     defaultValue: null,
   });
+  const inputFileVariants = clsx(
+    `file:bg-primary-green file:text-primary-white file:rounded-[3px] file:border-none w-[59vw] md:w-[29vw] lg:w-[22vw] 2xl:w-[18vw] xl:w-[17vw] file:cursor-pointer `,
+    {
+      "file:p-2": props.variant === "default",
+      "hidden p-3 opacity-0 -z-10": props.variant === "custom",
+    },
+  );
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -25,17 +32,37 @@ export const UploadField: FC<TUploadFile> = (props): ReactElement => {
           className={`${props.preview ? props.previewImage : "hidden"}`}
         />
       ) : (
-        <div className="w-auto h-auto">
+        <div className="w-auto h-auto ">
           <img src={props?.defaultImage || ""} alt="" className={props.previewImage} />
         </div>
       )}
-      <input
-        type="file"
-        accept="image/*,.pdf"
-        onChange={handleFileChange}
-        ref={ref}
-        className={props.classNameField}
-      />
+      {props.variant === "default" ? (
+        <input
+          type="file"
+          id="files"
+          accept="image/*,.pdf"
+          onChange={handleFileChange}
+          ref={ref}
+          className={inputFileVariants}
+        />
+      ) : (
+        <>
+          <input
+            type="file"
+            accept="image/*,.pdf"
+            id="files"
+            onChange={handleFileChange}
+            ref={ref}
+            className={inputFileVariants}
+          />
+          <label
+            htmlFor="files"
+            className="px-1 py-2 bg-primary-green text-md font- w-[59vw] md:w-[29vw] lg:w-[22vw] 2xl:w-[18vw] xl:w-full cursor-pointer text-primary-white text-center rounded z-10"
+          >
+            Choose File
+          </label>
+        </>
+      )}
     </div>
   );
 };
--- libs/web/modules/src/dashboard/user/dokumen/module.tsx
+++ libs/web/modules/src/dashboard/user/dokumen/module.tsx
@@ -28,14 +28,17 @@ export const ModuleDokumen: FC = (): ReactElement => {
     { label: "Pas Foto 3x4", name: "foto" },
     { label: "Rapor Sekolah", name: "Rapor" },
     {
-      label: "Dokumen Tambahan (Sertifikat, Penghargaan dll)",
+      label: "Dokumen Tambahan",
       name: "optionalDocument",
     },
     { label: "KIP-K", name: "kip" },
   ];
 
   return (
-    <form key="dashboard-dokumen" className="flex flex-col text-center px-4 gap-y-6  lg:text-start">
+    <form
+      key="dashboard-dokumen"
+      className="flex flex-col text-center overflow-x-hidden px-4 gap-y-6 relative lg:text-start"
+    >
       <div className="2xl:text-2xl">
         <h1 className="text-slate-5">
           PMB <span className="text-secondary-green-4"> / Upload Berkas</span>
@@ -46,9 +49,9 @@ export const ModuleDokumen: FC = (): ReactElement => {
       </div>
 
       {/* body */}
-      <section className="flex flex-col items-center shadow-lg w-[90vw] rounded-md h-[61rem] md:h-[35rem] mt-5 lg:w-[70vw] xl:w-[70vw]">
+      <section className="flex flex-col  shadow-lg w-[90vw] rounded-md h-[61rem] md:h-[35rem] mt-5 lg:w-[70vw] xl:w-[70vw]">
         {/* heading */}
-        <section className="flex flex-col items-center lg:items-start lg:pl-[2vw] gap-2 mt-5 lg:w-full">
+        <section className="flex flex-col lg:items-start lg:pl-[2vw] gap-2 mt-5 lg:w-full">
           <h1 className="font-bold text-[1.3rem] text-center">Upload berkas pendaftaran</h1>
           <h2 className="text-center text-grayscale-7 text-[11px] md:text-sm">
             Upload Berkas di bawah ini dengan format berikut : .jpg/.jpeg/.png/.pdf
@@ -56,30 +59,30 @@ export const ModuleDokumen: FC = (): ReactElement => {
         </section>
 
         {/* Document upload field */}
-        <section className="flex flex-col  h-auto items-center mt-5">
-          <div className="md:w-[80vw] lg:w-[66vw] xl:w-[66vw] md:h-auto md:flex ">
+        <section className="flex flex-col h-auto md:items-center mt-5">
+          <div className="md:w-[80vw] lg:w-[66vw] xl:w-[66vw] md:h-auto flex">
             <section className="flex flex-col gap-10 w-50% ">
               {personalDocument.map((documentType) => (
                 <div key={documentType.name} className="flex flex-col gap-2">
-                  <h3 className="font-bold">{documentType.label}</h3>
+                  <h3 className="font-bold text-xs md:text-base">{documentType.label}</h3>
                   <UploadField
                     control={control}
                     name={documentType.name}
-                    classNameField="file:bg-primary-green file:text-primary-white file:rounded-[3px] file:border-none w-[59vw] md:w-[29vw] lg:w-[22vw] xl:w-[18vw] xl:w-[17vw] file:cursor-pointer "
+                    variant="custom"
                     preview={false}
                   />
                 </div>
               ))}
             </section>
 
-            <section className="flex flex-col gap-10 w-[70vw] mt-10 md:mt-0 md:w-[60%] xl:w-full ">
+            <section className="flex flex-col gap-10 w-[70vw] md:mt-0 md:w-[60%] xl:w-full ">
               {graduationDocument.map((documentType) => (
                 <div key={documentType.name} className="flex flex-col gap-2 md:mt-[0.4vh]">
-                  <h3 className="font-bold text-[14.5px]">{documentType.label}</h3>
+                  <h3 className="font-bold text-xs md:text-base truncate">{documentType.label}</h3>
                   <UploadField
                     control={control}
                     name={documentType.name}
-                    classNameField="file:bg-primary-green file:text-primary-white file:rounded-sm file:border-none border-primary-green w-[59vw] md:w-[29vw] lg:w-[18vw] xl:w-[17vw] file:cursor-pointer "
+                    variant="custom"
                     preview={false}
                   />
                 </div>
@@ -89,8 +92,8 @@ export const ModuleDokumen: FC = (): ReactElement => {
         </section>
 
         {/* Button */}
-        <section className="w-[80vw] lg:w-[70vw] xl:w-[70vw] lg:pl-[2vw] flex justify-center lg:justify-end pr-16 items-center rounde">
-          <Button styling="bg-primary-green px-2 py-1 rounded-[3px] w-50% lg:w-25% xl:w-15% mt-8 md:mt-10">
+        <section className="w-full lg:w-[70vw] xl:w-[70vw] lg:pl-[2vw] flex justify-end mt-16 md:mt-10 items-center rounde pr-5">
+          <Button styling="bg-primary-green px-2 py-1 rounded-[3px] w-40% lg:w-25% xl:w-15% ">
             Submit Berkas
           </Button>
         </section>
