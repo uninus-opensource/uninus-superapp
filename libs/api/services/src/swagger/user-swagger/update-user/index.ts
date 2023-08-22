6d5ea2b1a9a9bf6e7036499fe75f70b337e2177d
YusronFauzanN <Yusronsronz7@gmail.com>
2023-08-22 15:21:31 +0700

    feature: add unit test for endpoint user and update endpoint user

--- libs/api/services/src/swagger/user-swagger/update-user/index.ts
+++ libs/api/services/src/swagger/user-swagger/update-user/index.ts
@@ -5,19 +5,10 @@ export class UpdateUserSwagger {
   public email!: string;
 
   @ApiProperty()
-  public nik!: string;
-
-  @ApiProperty()
   public fullname!: string;
 
   @ApiProperty({
     example: "min length 6, upper case 1, numbers 1",
   })
   public password!: string;
-
-  @ApiProperty()
-  public role_id!: number;
-
-  @ApiProperty()
-  public photo!: string;
 }
