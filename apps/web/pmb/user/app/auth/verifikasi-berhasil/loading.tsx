32a31320a0e2cb22b115ef03d6b2f15f7de1d768
EL5225 <luthfiramadhan155@gmail.com>
2023-08-27 18:34:48 +0700

    changes

--- apps/web/pmb/user/app/auth/verifikasi-berhasil/loading.tsx
+++ apps/web/pmb/user/app/auth/verifikasi-berhasil/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <section className="flex flex-col items-center justify-center w-full h-full gap-4 p-12 lg:pt-20">
-      <div className="rounded-full w-28 h-28 bg-grayscale-2 animate-pulse"></div>
+      <div className="rounded-full w-20 h-24 bg-grayscale-2 animate-pulse"></div>
       <div className="flex flex-col justify-center items-center gap-3 text-center">
         <div className="w-[11em] h-10 bg-grayscale-2 rounded-lg"></div>
         <div className="w-[70vw] md:w-[40vw] h-8 bg-grayscale-2 rounded-lg"></div>
