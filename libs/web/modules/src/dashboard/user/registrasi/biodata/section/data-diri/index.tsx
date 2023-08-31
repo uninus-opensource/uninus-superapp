7254612a622c68553227befdf1a7e3a07b72e057
EL5225 <luthfiramadhan155@gmail.com>
2023-08-31 09:47:40 +0700

    improvement select option

--- libs/web/modules/src/dashboard/user/registrasi/biodata/section/data-diri/index.tsx
+++ libs/web/modules/src/dashboard/user/registrasi/biodata/section/data-diri/index.tsx
@@ -7,7 +7,7 @@ import {
   Button,
   SelectField,
 } from "@uninus/web/components";
-import { formBiodataOne } from "../../store";
+import { dataDiri, disabilitiesDataDiri, formBiodataOne, occupationS2S3 } from "../../store";
 import { ChangeEvent, FC, ReactElement, useEffect, useMemo, useRef, useState } from "react";
 import { useForm, FieldValues } from "react-hook-form";
 import {
@@ -29,7 +29,7 @@ import { GroupBase, SelectInstance } from "react-select";
 import { TSelectOption } from "@uninus/web/components";
 import { useBiodataUpdate } from "../../hooks";
 import { ToastContainer, toast } from "react-toastify";
-import { VSDataDiri } from "./schema";
+import { VSDataDiri, TVSDataDiri } from "./schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { match } from "ts-pattern";
 import { EditOutlined } from "@ant-design/icons";
@@ -51,7 +51,7 @@ export const DataDiriSection: FC = (): ReactElement => {
     setValue,
     reset,
     formState: { errors },
-  } = useForm<FieldValues>({
+  } = useForm<FieldValues | TVSDataDiri>({
     resolver: zodResolver(VSDataDiri),
     mode: "all",
   });
@@ -322,30 +322,49 @@ export const DataDiriSection: FC = (): ReactElement => {
   const { mutate } = useBiodataUpdate();
 
   const onSubmit = handleSubmit((data) => {
+    dataDiri.fullname = data?.fullname;
+    dataDiri.nik = data?.nik;
+    dataDiri.nisn = data?.nisn;
+    dataDiri.no_kk = data?.no_kk;
+    dataDiri.gender_id = Number(data?.gender_id);
+    dataDiri.religion_id = Number(data?.religion_id);
+    dataDiri.birth_place = data?.birth_place;
+    dataDiri.birth_date = data?.birth_date;
+    dataDiri.marital_status_id = Number(data?.marital_status_id);
+    dataDiri.citizenship_id = Number(data?.citizenship_id);
+    dataDiri.country_id = Number(data?.country_id);
+    dataDiri.province_id = Number(data?.province_id);
+    dataDiri.city_id = Number(data?.city_id);
+    dataDiri.subdistrict_id = Number(data?.subdistrict_id);
+    dataDiri.address = data?.address;
+
+    if (disValue === "Ya") {
+      disabilitiesDataDiri.disabilities_id = Number(data?.disabilities_id);
+    } else {
+      disabilitiesDataDiri.disabilities_id = null;
+    }
+
+    if (occValue === "Sudah" && student?.degree_program_id !== 1) {
+      occupationS2S3.occupation_id = Number(data?.occupation_id);
+      occupationS2S3.occupation_position_id = Number(data?.occupation_position_id);
+      occupationS2S3.company_name = data?.company_name;
+      occupationS2S3.company_address = data?.company_address;
+      occupationS2S3.salary_id = Number(data?.salary_id);
+    }
+
     try {
+      console.log(data);
+      console.log({ ...dataDiri });
+      console.log({ ...occupationS2S3 });
+      console.log({ ...disabilitiesDataDiri });
       mutate(
-        match({
-          disable: disValue,
-          occ: occValue,
-          degreeId: student?.degree_program_id,
-        })
-          .with(
-            {
-              disable: "Tidak",
-              occ: "Sudah",
-              degreeId: 1,
-            },
-            () => {
-              return {
-                ...data,
-              };
-            },
-          )
-          .otherwise(() => {
-            return {
-              ...data,
-            };
-          }),
+        occValue === "Sudah" && student?.degree_program_id !== 1
+          ? { ...dataDiri, ...occupationS2S3 }
+          : occValue === "Sudah" && disValue === "Ya" && student?.degree_program_id !== 1
+          ? { ...dataDiri, ...occupationS2S3, ...disabilitiesDataDiri }
+          : occValue === null && student?.degree_program_id === 1 && disValue === "Ya"
+          ? { ...dataDiri, ...disabilitiesDataDiri }
+          : { ...dataDiri },
         {
           onSuccess: () => {
             setIsdisabled(true);
@@ -525,6 +544,7 @@ export const DataDiriSection: FC = (): ReactElement => {
             disabled={isDisabled || !!student?.gender_id}
             status={"error"}
             size={"md"}
+            message={errors?.gender_id?.message as string}
           />
 
           <SelectOption
@@ -546,6 +566,7 @@ export const DataDiriSection: FC = (): ReactElement => {
             disabled={isDisabled || !!student?.religion_id}
             status={"error"}
             size={"md"}
+            message={errors?.religion_id?.message as string}
           />
 
           <TextField
@@ -574,6 +595,7 @@ export const DataDiriSection: FC = (): ReactElement => {
             inputWidth="lg:w-[27vw] xl:w-[25vw] md:w-[33vw] w-[70vw]"
             control={control}
             disabled={isDisabled || !!student?.birth_date}
+            message={errors?.birth_date?.message as string}
           />
 
           <SelectOption
@@ -595,6 +617,7 @@ export const DataDiriSection: FC = (): ReactElement => {
             disabled={isDisabled || !!student?.marital_status_id}
             status={"error"}
             size={"md"}
+            message={errors?.marital_status_id?.message as string}
           />
 
           <SelectOption
@@ -617,6 +640,7 @@ export const DataDiriSection: FC = (): ReactElement => {
             disabled={isDisabled || !!student?.citizenship_id}
             status={"error"}
             size={"md"}
+            message={errors?.citizenship_id?.message as string}
           />
           <SelectOption
             name="country_id"
@@ -638,6 +662,7 @@ export const DataDiriSection: FC = (): ReactElement => {
             disabled={isDisabled || !!student?.country_id || !watch("citizenship_id")}
             status={"error"}
             size={"md"}
+            message={errors?.country_id?.message as string}
           />
           <SelectOption
             name="province_id"
@@ -657,8 +682,9 @@ export const DataDiriSection: FC = (): ReactElement => {
             ref={citizenref}
             isMulti={false}
             disabled={isDisabled || !!student?.province_id || countryOptions?.length !== 1}
-            status={"error"}
+            status={errors?.province_id?.message ? "error" : "none"}
             size={"md"}
+            message={errors?.province_id?.message as string}
           />
           <SelectOption
             name="city_id"
@@ -683,6 +709,7 @@ export const DataDiriSection: FC = (): ReactElement => {
             }
             status={"error"}
             size={"md"}
+            message={errors?.city_id?.message as string}
           />
           <SelectOption
             name="subdistrict_id"
@@ -709,6 +736,7 @@ export const DataDiriSection: FC = (): ReactElement => {
             }
             status={"error"}
             size={"md"}
+            message={errors?.subdistrict_id?.message as string}
           />
 
           <div className="px-6 md:px-0 lg:px-0 w-full">
@@ -786,6 +814,7 @@ export const DataDiriSection: FC = (): ReactElement => {
                   }
                   status={"error"}
                   size={"md"}
+                  message={errors?.occupation_id?.message as string}
                 />
                 <SelectOption
                   name="occupation_position_id"
@@ -816,6 +845,7 @@ export const DataDiriSection: FC = (): ReactElement => {
                   }
                   status={"error"}
                   size={"md"}
+                  message={errors?.occupation_position_id?.message as string}
                 />
                 <TextField
                   inputHeight="h-10"
--- libs/web/modules/src/dashboard/user/registrasi/biodata/section/data-diri/schema.ts
+++ libs/web/modules/src/dashboard/user/registrasi/biodata/section/data-diri/schema.ts
@@ -2,9 +2,9 @@ import { z } from "zod";
 
 export const VSDataDiri = z.object({
   avatar: z.any().optional(),
-  fullname: z.string().optional().nullable(),
-  email: z.string().optional().nullable(),
-  phone_number: z.string().optional().nullable(),
+  fullname: z.string().optional(),
+  email: z.string().optional(),
+  phone_number: z.string().optional(),
   nik: z
     .string({
       required_error: "NIK harus diisi",
@@ -26,84 +26,60 @@ export const VSDataDiri = z.object({
       message: "NISN harus angka",
     }),
   no_kk: z
-    .string()
+    .string({
+      required_error: "No KK harus diisi",
+    })
     .nonempty({ message: "Nomor KK harus diisi" })
     .min(16, { message: "Nomor KK harus 16 karakter" })
     .max(16, { message: "Nomor KK harusl 16 karakter" })
     .refine((data) => data.match(/[0-9]/g), {
       message: "Nomor KK harus angka",
     }),
-  birth_place: z.string().nonempty({ message: "Tempat lahir harus diisi" }).nullable(),
-  birth_date: z.string().nonempty({ message: "Tanggal lahir harus diisi" }).nullable(),
-  gender_id: z
-    .number()
-    .min(1, { message: "Jenis kelamin harus diisi" })
-    .max(1, {
-      message: "Jenis kelamin harus diisi",
-    })
-    .optional()
-    .nullable(),
-  martial_status_id: z
-    .number()
-    .min(1, { message: "Status pernikahan harus diisi" })
-    .max(1, { message: "Status pernikahan harus diisi" }),
-  religion_id: z
-    .number()
-    .min(1, { message: "Agama harus diisi" })
-    .max(1, {
-      message: "Agama harus diisi",
-    })
-    .nullable()
-    .optional(),
-  citizenship_id: z
-    .number()
-    .min(1, { message: "Kewarganegaraan harus diisi" })
-    .max(1, {
-      message: "Kewarganegaraan harus diisi",
-    })
-    .nullable()
-    .optional(),
-  country_id: z
-    .number()
-    .min(1, { message: "Negara asal harus diisi" })
-    .max(1, {
-      message: "Negara asal harus diisi",
-    })
-    .nullable()
-    .optional(),
-  province_id: z
-    .number()
-    .min(1, { message: "Provinsi harus diisi" })
-    .max(1, {
-      message: "Provinsi harus diisi",
-    })
-    .nullable()
-    .optional(),
-  city_id: z
-    .number()
-    .min(1, { message: "Kota harus diisi" })
-    .max(1, {
-      message: "Kota harus diisi",
-    })
-    .nullable()
-    .optional(),
-  subdistrict_id: z
-    .number()
-    .min(1, { message: "Kecamatan harus diisi" })
-    .max(1, {
-      message: "Kecamatan harus diisi",
-    })
-    .nullable()
-    .optional(),
-  address: z.string().nonempty({ message: "Alamat harus diisi" }).nullable().optional(),
-  disabilities_id: z.number().int().optional().nullable(),
-  occupation_id: z.number().int().optional().nullable(),
-  occupation_position_id: z.number().int().optional().nullable(),
+  birth_place: z.nullable(
+    z
+      .string({ required_error: "Tempat lahir harus diisi" })
+      .nonempty({ message: "Tempat lahir harus diisi" }),
+  ),
+  birth_date: z.string().nonempty({ message: "Tanggal lahir harus diisi" }),
+  gender_id: z.any().refine((value) => value, {
+    message: "Jenis kelamin harus diisi",
+  }),
+
+  marital_status_id: z.any().refine((value) => value, {
+    message: "Status harus diisi",
+  }),
+  religion_id: z.any().refine((value) => value, {
+    message: "Agama harus diisi",
+  }),
+
+  citizenship_id: z.any().refine((value) => value, {
+    message: "Kewarganegaraan harus diisi",
+  }),
+
+  country_id: z.any().refine((value) => value, {
+    message: "Asal Negara harus diisi",
+  }),
+
+  province_id: z.any().refine((value) => value, {
+    message: "Provinsi harus diisi",
+  }),
+
+  city_id: z.any().refine((value) => value, {
+    message: "Kota harus diisi",
+  }),
+
+  subdistrict_id: z.any().refine((value) => value, {
+    message: "Kecamatan harus diisi",
+  }),
+
+  address: z.nullable(z.string().nonempty({ message: "Alamat harus diisi" })),
+
+  disabilities_id: z.string().nullable().optional(),
+  occupation_id: z.string().nullable().optional(),
+  occupation_position_id: z.string().nullable().optional(),
   company_name: z.string().optional(),
   company_address: z.string().optional(),
-  salary_id: z.number().int().optional().nullable(),
-  marital_status_id: z.number().int().optional().nullable(),
-  degree_program_id: z.number().int().optional().nullable(),
+  salary_id: z.string().nullable().optional(),
 });
 
 export type TVSDataDiri = z.infer<typeof VSDataDiri>;
