ee0a3bf26aeb4830bde66770f2dedad56dce7f7a
Muhammad Luthfi Ramadhan <108562600+EL5225@users.noreply.github.com>
2023-08-31 11:03:46 +0700

    improvement select option (#967)
    * improvement select option
    
    * fix changes

--- libs/web/providers/src/query/index.tsx
+++ libs/web/providers/src/query/index.tsx
@@ -3,6 +3,13 @@ import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { FC, PropsWithChildren, ReactElement } from "react";
 
 export const QueryProvider: FC<PropsWithChildren> = ({ children }): ReactElement => {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 15 * (60 * 1000),
+        cacheTime: 20 * (60 * 1000),
+      },
+    },
+  });
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
