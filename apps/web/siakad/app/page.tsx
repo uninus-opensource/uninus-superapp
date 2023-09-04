6125d7b32270c8dade5ab8f80d9b24a59b84e175
Rohendo <108710826+RohendoJ@users.noreply.github.com>
2023-09-04 14:27:53 +0700

    Improvement/login siakad page (#1033)
    * improvement: change http method put -> patch
    
    * improvement: siakad-login-page

--- apps/web/siakad/app/page.tsx
+++ apps/web/siakad/app/page.tsx
@@ -1,15 +1,11 @@
 import { ReactElement } from "react";
 import { NextPage } from "next";
 import { Metadata } from "next";
-import { LoginAdminModule } from "@uninus/web/modules";
+import { LoginSiakadModule } from "@uninus/web/modules";
 
 export const metadata: Metadata = {
   title: "Siakad",
 };
 
-const LandingSiakad: NextPage = (): ReactElement => (
-  <section key="landing-siakad">
-    <LoginAdminModule />
-  </section>
-);
-export default LandingSiakad;
+const SiakadPage: NextPage = (): ReactElement => <LoginSiakadModule key="auth-siakad" />;
+export default SiakadPage;
