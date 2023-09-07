ac4fb859805811c408712fecd1eb5fc5cabf167a
sugiartofahmi <sugiartofahmi@gmail.com>
2023-09-07 23:51:16 +0700

    improvement : handle status code RpcException

--- libs/api/filter/src/rpcexception/rpcToHttp.filter.ts
+++ libs/api/filter/src/rpcexception/rpcToHttp.filter.ts
@@ -1,4 +1,4 @@
-import { Catch, ArgumentsHost, ExceptionFilter } from "@nestjs/common";
+import { Catch, ArgumentsHost, ExceptionFilter, HttpStatus } from "@nestjs/common";
 import { RpcException } from "@nestjs/microservices";
 
 @Catch(RpcException)
@@ -6,10 +6,9 @@ export class RpcExceptionToHttpExceptionFilter implements ExceptionFilter {
   catch(exception: RpcException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
-    const error: any = exception.getError();
-    response.status(error.statusCode).json({
-      statusCode: error.statusCode,
-      message: error.message,
-    });
+    const error = Object.values(exception.getError());
+    const statusCode =
+      error.length === 3 && typeof error[2] === "number" ? error[2] : HttpStatus.BAD_REQUEST;
+    response.status(statusCode).json(exception.getError());
   }
 }
