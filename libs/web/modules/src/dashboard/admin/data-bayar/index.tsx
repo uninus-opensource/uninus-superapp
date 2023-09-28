e3e760e91840ffe94bb91f80430bf5194371cdd9
Muhammad Luthfi Ramadhan <108562600+EL5225@users.noreply.github.com>
2023-09-28 23:28:01 +0700

    feature: access role dashboard (#1237)

--- libs/web/modules/src/dashboard/admin/data-bayar/index.tsx
+++ libs/web/modules/src/dashboard/admin/data-bayar/index.tsx
@@ -1,8 +1,15 @@
 "use client";
 import { ReactElement, FC } from "react";
 import Table from "./table";
+import { redirect } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 export const ModuleDataBayar: FC = (): ReactElement => {
+  const { data: session } = useSession();
+
+  if (session?.user.role !== "Super Admin PMB") {
+    redirect("/dashboard");
+  }
   return (
     <section className="flex flex-col text-center gap-y-6 lg:text-start">
       <div className="2xl:text-2xl">
--- libs/web/modules/src/dashboard/admin/data-master/index.tsx
+++ libs/web/modules/src/dashboard/admin/data-master/index.tsx
@@ -1,8 +1,15 @@
 "use client";
 import { ReactElement, FC } from "react";
 import Table from "./table";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 export const ModuleDataMaster: FC = (): ReactElement => {
+  const { data: session } = useSession();
+
+  if (session?.user.role !== "Super Admin PMB") {
+    redirect("/dashboard");
+  }
   return (
     <section className="flex flex-col text-center gap-y-6 lg:text-start">
       <div className="2xl:text-2xl">
