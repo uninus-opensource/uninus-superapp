2cf407a7e84fe60ffc607c66f1af2683236b2088
Fenny Oktaviani <79123882+fnny04@users.noreply.github.com>
2023-10-04 16:12:27 +0700

    Improvement : design dashboard user (#1299)
    * Improvement : Dashboard User PMB
    
    * Imprvement : add assets and fix deign card

--- libs/web/modules/src/dashboard/user/module.tsx
+++ libs/web/modules/src/dashboard/user/module.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { ReactElement, FC } from "react";
 import Image from "next/image";
-import { ArrowLine } from "@uninus/web/components";
+import { ArrowDownOutlined, ArrowLeftOutlined, ArrowRightOutlined } from "@ant-design/icons";
 
 export const DashboardModule: FC = (): ReactElement => {
   return (
@@ -12,43 +12,46 @@ export const DashboardModule: FC = (): ReactElement => {
         </h1>
         <p className=" text-lg 2xl:text-2xl pt-2 font-extrabold text-secondary-green-4">Beranda</p>
       </div>
+      <div className="flex w-full h-[150px] bg-[#FBFDE9] rounded-md p-4">
+        <Image
+          src={"/illustrations/warning.svg"}
+          width={200}
+          height={100}
+          priority
+          quality={100}
+          alt={"neo-uninus"}
+          className="w-30 p-4"
+        />
+        <div>
+          <h1 className="font-bold">PERINGATAN</h1>
+          <p className="text-sm">
+            Untuk mempermudah proses PMB, harap mempersiapkan dokumen - dokumen berikut:
+            <br />
+            S1 : Ijazah, Akte, Ktp, KK <br />
+            S2 : Ijazah S1, Ktp, KK, Porlap dikti, Transkip nilai S1 <br />
+            S3 : Ijazah S2, Ktp, KK, Porlap dikti, Transkip nilai S2
+          </p>
+        </div>
+      </div>
 
       {/* body */}
-      <div className="flex lg:flex-row md:flex-row flex-col gap-4 w-full bg-primary-white p-4 rounded-lg shadow-lg ">
-        <div className="flex flex-col md:w-[50%] sm:w-full lg:pb-20 lg:pl-8 items-center justify-center">
-          {/* Image */}
-          <figure className=" grid place-items-center ">
-            <Image
-              src={"/illustrations/dashboard-home.webp"}
-              alt="home"
-              width={292}
-              height={300}
-              quality={100}
-              priority
-              className="w-[120px] h-[120px] md:w-[200px] md:h-[200px] lg:pl-8 object-cover"
-            />
-          </figure>
-
-          {/* heading */}
-          <div className="flex flex-col gap-1 justify-center items-center w-full text-center ">
-            <h1 className="text-[20px] text-primary-green font-bold  w-full">
-              Selamat Datang Di Dashboard PMB Universitas Islam Nusantara
-            </h1>
-            <div className=" w-full">
-              <h2 className="text-[12px] lg:text-base ">
-                Untuk melakukan pendaftaran dapat mengikuti tata cara berikut :
-              </h2>
-            </div>
-          </div>
+      <div className="flex flex-col gap-4 w-full bg-primary-white p-4 rounded-lg shadow-lg ">
+        <div className="flex flex-col justify-start w-full text-left px-12 ">
+          <h1 className="text-[20px] text-primary-green font-bold  w-full">
+            Selamat Datang Di Dashboard PMB <br /> Universitas Islam Nusantara
+          </h1>{" "}
+          <p className=" text-sm py-2">
+            Untuk melakukan pendaftaran dapat mengikuti tata cara berikut :
+          </p>
         </div>
 
         {/* flow register */}
-        <div className=" md:w-[50%] sm:w-full xl:w-[55%] h-auto">
-          <div className=" w-full h-auto flex flex-col justify-center items-center">
+        <div className=" md:w-[50%] sm:w-full lg:w-full h-auto px-12">
+          <div className=" w-full flex justify-between xl:flex-row items-center p-4 gap-4">
             <div className="lg:w-[300px] md:w-[300px] w-full h-auto bg-primary-white shadow-md grid place-items-start">
               <div className="flex text-left lg:w-[80%] w-full lg:p-4 p-2">
                 <h1 className="text-grayscale-9 text-[9.5px] md:text-[10px] xl:text-[12px]">
-                  <span className="text-primary-green font-semibold md:text-[11px] xl:text-[13px]">
+                  <span className="text-primary-green font-bold md:text-[11px] xl:text-[13px]">
                     Halaman Formulir :
                   </span>{" "}
                   <br />
@@ -57,23 +60,30 @@ export const DashboardModule: FC = (): ReactElement => {
                 </h1>
               </div>
             </div>
-            <ArrowLine />
+            <div className="text-primary-green">
+              <ArrowRightOutlined />
+            </div>
             <div className="lg:w-[300px] md:w-[300px] w-full h-auto bg-primary-white shadow-md grid place-items-start">
               <div className="flex text-left lg:w-[80%] w-full lg:p-4 p-2">
                 <h1 className="text-grayscale-9 text-[9.5px] md:text-[10px] xl:text-[12px]">
-                  <span className="text-primary-green font-semibold md:text-[11px] xl:text-[13px]">
+                  <span className="text-primary-green font-bold md:text-[11px] xl:text-[13px]">
                     Halaman Registrasi :
                   </span>{" "}
                   <br />
-                  Mengisi semua data registrasi yang diperlukan, dan melakukan pembayaran.
+                  Mengisi semua data registrasi yang diperlukan.
                 </h1>
               </div>
             </div>
-            <ArrowLine />
+          </div>
+          <div className="flex pr-40 text-primary-green w-full justify-end">
+            <ArrowDownOutlined />
+          </div>
+
+          <div className=" w-full justify-between xl:flex-row items-center flex p-4 gap-4">
             <div className="lg:w-[300px] md:w-[300px] w-full h-auto bg-primary-white shadow-md grid place-items-start">
               <div className="flex text-left lg:w-[80%] w-full lg:p-4 p-2">
                 <h1 className="text-grayscale-9 text-[9.5px] md:text-[10px] xl:text-[12px]">
-                  <span className="text-primary-green font-semibold md:text-[11px] xl:text-[13px]">
+                  <span className="text-primary-green font-bold md:text-[11px] xl:text-[13px]">
                     Halaman Upload Berkas :
                   </span>{" "}
                   <br />
@@ -85,11 +95,30 @@ export const DashboardModule: FC = (): ReactElement => {
                 </h1>
               </div>
             </div>
-            <ArrowLine />
+            <div className="text-primary-green">
+              <ArrowLeftOutlined />
+            </div>
             <div className="lg:w-[300px] md:w-[300px] w-full h-auto bg-primary-white shadow-md grid place-items-start">
               <div className="flex text-left lg:w-[80%] w-full lg:p-4 p-2">
                 <h1 className="text-grayscale-9 text-[9.5px] md:text-[10px] xl:text-[12px]">
-                  <span className="text-primary-green font-semibold md:text-[11px] xl:text-[13px]">
+                  <span className="text-primary-green font-bold md:text-[11px] xl:text-[13px]">
+                    Melakukan Pembayaran :
+                  </span>{" "}
+                  <br />
+                  Setelah semua tahap dilakukan, segera lakukan pembayaran registrasi
+                </h1>
+              </div>
+            </div>
+          </div>
+          <div className="text-primary-green pl-40">
+            <ArrowDownOutlined />
+          </div>
+
+          <div className=" w-full justify-between items-center flex p-4 gap-4">
+            <div className="lg:w-[300px] md:w-[300px] w-full h-auto bg-primary-white shadow-md grid place-items-start">
+              <div className="flex text-left lg:w-[80%] w-full lg:p-4 p-2">
+                <h1 className="text-grayscale-9 text-[9.5px] md:text-[10px] xl:text-[12px]">
+                  <span className="text-primary-green font-bold md:text-[11px] xl:text-[13px]">
                     Halaman Tes Seleksi :
                   </span>{" "}
                   <br />
@@ -97,11 +126,13 @@ export const DashboardModule: FC = (): ReactElement => {
                 </h1>
               </div>
             </div>
-            <ArrowLine />
-            <div className="lg:w-[300px] md:w-[300px] w-full h-auto bg-primary-white shadow-md grid place-items-start">
-              <div className="flex text-left lg:w-[80%] w-full lg:p-4 p-2">
+            <div className="text-primary-green">
+              <ArrowRightOutlined />
+            </div>
+            <div className="lg:w-[300px] md:w-[300px] w-full h-auto bg-primary-green shadow-md grid justify-center">
+              <div className="flex text-center w-full lg:p-4 p-2">
                 <h1 className="text-grayscale-9 text-[9.5px] md:text-[10px] xl:text-[12px]">
-                  <span className="text-primary-green font-semibold md:text-[11px] xl:text-[13px]">
+                  <span className=" text-primary-white font-bold md:text-[11px] xl:text-[13px]">
                     Pendaftaran PMB UNINUS selesai, dan menunggu hasil seleksi
                   </span>
                 </h1>
