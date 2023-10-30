eb9a91ce0474ab40bb0e10aeb4db4ac342dde8a1
YusronFauzanN <Yusronsronz7@gmail.com>
2023-10-30 15:06:34 +0700

    improvement: update response api contract bendahara penerimaan

--- apps/api/finance/src/app/app.controller.ts
+++ apps/api/finance/src/app/app.controller.ts
@@ -1,16 +1,19 @@
 import { Controller } from "@nestjs/common";
-
 import { AppService } from "./app.service";
 import { MessagePattern } from "@nestjs/microservices";
-import { TCreatePaymentRequest, TStatusPaymentRequest } from "@uninus/entities";
+import {
+  TCreatePaymentRequest,
+  TStatusPaymentRequest,
+  TFinanceSummaryRequest,
+} from "@uninus/entities";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @MessagePattern("get_finance_summary")
-  getFinanceSummary() {
-    return this.appService.getFinanceSummary();
+  @MessagePattern("get_data_finance_summary")
+  async getFinanceSummary(payload: TFinanceSummaryRequest) {
+    return await this.appService.getFinanceSummary(payload);
   }
 
   @MessagePattern("request_payment")
--- libs/api/controllers/src/finance/index.ts
+++ libs/api/controllers/src/finance/index.ts
@@ -1,22 +1,30 @@
-import { Controller, Get, Inject, Post, UseFilters, Body, UseGuards } from "@nestjs/common";
 import { ClientProxy, RpcException } from "@nestjs/microservices";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiQuery, ApiTags } from "@nestjs/swagger";
 import { CreatePaymentDto, StatusPaymentDto } from "@uninus/api/dto";
 import { JwtAuthGuard } from "@uninus/api/guard";
 import { RpcExceptionToHttpExceptionFilter } from "@uninus/api/pipes";
 import { catchError, firstValueFrom, throwError } from "rxjs";
+import { Controller, Get, Inject, Post, Body, UseGuards, Query, UseFilters } from "@nestjs/common";
 
 @Controller("finance")
 @ApiTags("Finance")
 export class FinanceController {
   constructor(@Inject("FINANCE_SERVICE") private readonly client: ClientProxy) {}
 
-  @Get()
+  @Get("/payment-summary")
+  @ApiOperation({ summary: "Get Data Finance Summary" })
+  @ApiQuery({ name: "filter", required: false })
+  @ApiQuery({ name: "start_date", required: false })
+  @ApiQuery({ name: "end_date", required: false })
   @UseFilters(new RpcExceptionToHttpExceptionFilter())
-  async financeSummary() {
+  async financeSummary(
+    @Query("filter") filter: string,
+    @Query("start_date") start_date: string,
+    @Query("end_date") end_date: string,
+  ) {
     const response = await firstValueFrom(
       this.client
-        .send("get_finance_summary", {})
+        .send("get_data_finance_summary", { filter, start_date, end_date })
         .pipe(catchError((error) => throwError(() => new RpcException(error.response)))),
     );
     return response;
