d8f9d17a3ae306c1007c30dd772f6fcd54eacb03
sugiartofahmi <sugiartofahmi@gmail.com>
2023-11-27 06:40:05 +0700

    improvement: add try catch auth service

--- apps/api/auth/src/app/app.service.ts
+++ apps/api/auth/src/app/app.service.ts
@@ -42,140 +42,144 @@ export class AppService {
   constructor(private prisma: PrismaService) {}
 
   async register(payload: TRegisterRequest): Promise<TRegisterResponse> {
-    const isUserExist = await this.prisma.users.findMany({
-      where: {
-        OR: [
-          {
-            email: payload.email,
-          },
-          {
-            students: {
-              phone_number: `62${payload.phone_number}`,
+    try {
+      const isUserExist = await this.prisma.users.findMany({
+        where: {
+          OR: [
+            {
+              email: payload.email,
+            },
+            {
+              students: {
+                phone_number: `62${payload.phone_number}`,
+              },
+            },
+          ],
+        },
+      });
+
+      if (isUserExist.length) {
+        throw new ConflictException("Email atau nomor telepon sudah terdaftar");
+      }
+
+      const password = await encryptPassword(payload.password);
+      const now = new Date();
+      const year = now.getFullYear().toString();
+      const month = (now.getMonth() + 1).toString().padStart(2, "0");
+      const day = now.getDate().toString().padStart(2, "0");
+
+      const lastRegistration = await this.prisma.pMB.findFirst({
+        orderBy: { registration_number: "desc" },
+      });
+
+      let registrationCounter = 1;
+      if (lastRegistration) {
+        registrationCounter = parseInt(lastRegistration.registration_number.substring(8)) + 1;
+      }
+
+      const formattedCounter = registrationCounter.toString().padStart(6, "0");
+
+      const registrationNumber = `${year}${month}${day}${formattedCounter}`;
+      const { token, expiredAt } = await generateOtp();
+      const createUser = await this.prisma.users.create({
+        data: {
+          fullname: payload.fullname,
+          email: payload.email.toLowerCase(),
+          password,
+          otp: {
+            create: {
+              token,
+              expiredAt,
             },
           },
-        ],
-      },
-    });
-
-    if (isUserExist.length) {
-      throw new RpcException(new ConflictException("Email atau nomor telepon sudah terdaftar"));
-    }
-
-    const password = await encryptPassword(payload.password);
-    const now = new Date();
-    const year = now.getFullYear().toString();
-    const month = (now.getMonth() + 1).toString().padStart(2, "0");
-    const day = now.getDate().toString().padStart(2, "0");
-
-    const lastRegistration = await this.prisma.pMB.findFirst({
-      orderBy: { registration_number: "desc" },
-    });
-
-    let registrationCounter = 1;
-    if (lastRegistration) {
-      registrationCounter = parseInt(lastRegistration.registration_number.substring(8)) + 1;
-    }
-
-    const formattedCounter = registrationCounter.toString().padStart(6, "0");
-
-    const registrationNumber = `${year}${month}${day}${formattedCounter}`;
-    const { token, expiredAt } = await generateOtp();
-    const createUser = await this.prisma.users.create({
-      data: {
-        fullname: payload.fullname,
-        email: payload.email.toLowerCase(),
-        password,
-        otp: {
-          create: {
-            token,
-            expiredAt,
-          },
-        },
-        avatar: "https://uninus-demo.s3.ap-southeast-1.amazonaws.com/avatar-default.png",
-        students: {
-          create: {
-            phone_number: `62${payload.phone_number}`,
-            pmb: {
-              create: {
-                registration_number: registrationNumber,
-                registration_status_id: 1,
-                student_grade: {
-                  createMany: {
-                    data: [
-                      {
-                        subject: "indonesia",
-                        semester: "1",
-                      },
-                      {
-                        subject: "indonesia",
-                        semester: "2",
-                      },
-                      {
-                        subject: "indonesia",
-                        semester: "3",
-                      },
-                      {
-                        subject: "indonesia",
-                        semester: "4",
-                      },
-                      {
-                        subject: "matematika",
-                        semester: "1",
-                      },
-                      {
-                        subject: "matematika",
-                        semester: "2",
-                      },
-                      {
-                        subject: "matematika",
-                        semester: "3",
-                      },
-                      {
-                        subject: "matematika",
-                        semester: "4",
-                      },
-                      {
-                        subject: "inggris",
-                        semester: "1",
-                      },
-                      {
-                        subject: "inggris",
-                        semester: "2",
-                      },
-                      {
-                        subject: "inggris",
-                        semester: "3",
-                      },
-                      {
-                        subject: "inggris",
-                        semester: "4",
-                      },
-                    ],
+          avatar: "https://uninus-demo.s3.ap-southeast-1.amazonaws.com/avatar-default.png",
+          students: {
+            create: {
+              phone_number: `62${payload.phone_number}`,
+              pmb: {
+                create: {
+                  registration_number: registrationNumber,
+                  registration_status_id: 1,
+                  student_grade: {
+                    createMany: {
+                      data: [
+                        {
+                          subject: "indonesia",
+                          semester: "1",
+                        },
+                        {
+                          subject: "indonesia",
+                          semester: "2",
+                        },
+                        {
+                          subject: "indonesia",
+                          semester: "3",
+                        },
+                        {
+                          subject: "indonesia",
+                          semester: "4",
+                        },
+                        {
+                          subject: "matematika",
+                          semester: "1",
+                        },
+                        {
+                          subject: "matematika",
+                          semester: "2",
+                        },
+                        {
+                          subject: "matematika",
+                          semester: "3",
+                        },
+                        {
+                          subject: "matematika",
+                          semester: "4",
+                        },
+                        {
+                          subject: "inggris",
+                          semester: "1",
+                        },
+                        {
+                          subject: "inggris",
+                          semester: "2",
+                        },
+                        {
+                          subject: "inggris",
+                          semester: "3",
+                        },
+                        {
+                          subject: "inggris",
+                          semester: "4",
+                        },
+                      ],
+                    },
                   },
                 },
               },
             },
           },
         },
-      },
-      select: {
-        fullname: true,
-        otp: {
-          select: {
-            token: true,
+        select: {
+          fullname: true,
+          otp: {
+            select: {
+              token: true,
+            },
           },
         },
-      },
-    });
+      });
 
-    if (!createUser) {
-      throw new RpcException(new BadRequestException("Gagal membuat akun"));
+      if (!createUser) {
+        throw new BadRequestException("Gagal membuat akun");
+      }
+
+      return {
+        fullname: createUser.fullname,
+        otp: createUser.otp.token,
+      };
+    } catch (error) {
+      throw new RpcException(errorMappings(error));
     }
-
-    return {
-      fullname: createUser.fullname,
-      otp: createUser.otp.token,
-    };
   }
 
   async login(payload: TLoginRequest): Promise<TLoginResponse> {
@@ -251,191 +255,223 @@ export class AppService {
   }
 
   async logout(payload: TLogoutRequest): Promise<TLogoutResponse> {
-    const result = await this.prisma.users.updateMany({
-      where: {
-        refresh_token: payload.refresh_token,
-      },
-      data: {
-        refresh_token: null,
-      },
-    });
+    try {
+      const result = await this.prisma.users.updateMany({
+        where: {
+          refresh_token: payload.refresh_token,
+        },
+        data: {
+          refresh_token: null,
+        },
+      });
 
-    if (!result) {
-      throw new RpcException(new UnauthorizedException("Gagal Logout"));
+      if (!result) {
+        throw new UnauthorizedException("Gagal Logout");
+      }
+
+      return {
+        message: "Berhasil logout",
+      };
+    } catch (error) {
+      throw new RpcException(errorMappings(error));
     }
-
-    return {
-      message: "Berhasil logout",
-    };
   }
 
   async getUserByEmail(payload: TUserEmail): Promise<TUserEmailResponse> {
-    const user = await this.prisma.users.findUnique({
-      where: {
-        email: payload.email,
-      },
-      select: {
-        id: true,
-        email: true,
-        fullname: true,
-        otp: {
-          select: {
-            token: true,
+    try {
+      const user = await this.prisma.users.findUnique({
+        where: {
+          email: payload.email,
+        },
+        select: {
+          id: true,
+          email: true,
+          fullname: true,
+          otp: {
+            select: {
+              token: true,
+            },
           },
         },
-      },
-    });
-    if (!user) {
-      throw new RpcException(new NotFoundException("Email tidak ditemukan"));
-    }
+      });
+      if (!user) {
+        throw new NotFoundException("Email tidak ditemukan");
+      }
 
-    return { id: user.id, email: user.email, otp: user.otp?.token };
+      return { id: user.id, email: user.email, otp: user.otp?.token };
+    } catch (error) {
+      throw new RpcException(errorMappings(error));
+    }
   }
 
   async refreshToken({ user }: TReqToken): Promise<TResRefreshToken> {
-    const expiresIn = 15 * 60 * 1000;
-    const access_token = await generateAccessToken(user);
+    try {
+      const expiresIn = 15 * 60 * 1000;
+      const access_token = await generateAccessToken(user);
 
-    const now = Date.now();
-    const expirationTime = now + expiresIn;
+      const now = Date.now();
+      const expirationTime = now + expiresIn;
 
-    if (now > expirationTime) {
-      throw new RpcException(new UnauthorizedException("Access Token telah berakhir"));
+      return {
+        access_token,
+        exp: expirationTime,
+      };
+    } catch (error) {
+      throw new RpcException(errorMappings(error));
     }
-
-    return {
-      access_token,
-      exp: expirationTime,
-    };
   }
 
   async createOtpUser(payload: TUserEmail) {
-    await this.clearOtp();
-    await this.getUserByEmail(payload);
-    const { token, expiredAt } = await generateOtp();
-    const createOtp = await this.prisma.users.update({
-      where: {
-        email: payload.email,
-      },
-      data: {
-        otp: {
-          upsert: {
-            update: {
-              token,
-              expiredAt,
-            },
-            create: {
-              token,
-              expiredAt,
+    try {
+      await this.clearOtp();
+      await this.getUserByEmail(payload);
+      const { token, expiredAt } = await generateOtp();
+      const createOtp = await this.prisma.users.update({
+        where: {
+          email: payload.email,
+        },
+        data: {
+          otp: {
+            upsert: {
+              update: {
+                token,
+                expiredAt,
+              },
+              create: {
+                token,
+                expiredAt,
+              },
             },
           },
         },
-      },
-      select: {
-        fullname: true,
-        otp: {
-          select: {
-            token: true,
+        select: {
+          fullname: true,
+          otp: {
+            select: {
+              token: true,
+            },
           },
         },
-      },
-    });
-    if (!createOtp) {
-      throw new RpcException(new BadRequestException("Gagal saat generate OTP"));
+      });
+      if (!createOtp) {
+        throw new BadRequestException("Gagal saat generate OTP");
+      }
+      return {
+        fullname: createOtp.fullname,
+        otp: createOtp.otp?.token,
+      };
+    } catch (error) {
+      throw new RpcException(errorMappings(error));
     }
-    return {
-      fullname: createOtp.fullname,
-      otp: createOtp.otp?.token,
-    };
   }
 
   async clearOtp() {
-    const clearOtp = await this.prisma.oTP.deleteMany({
-      where: {
-        expiredAt: {
-          lte: new Date().getTime(),
+    try {
+      const clearOtp = await this.prisma.oTP.deleteMany({
+        where: {
+          expiredAt: {
+            lte: new Date().getTime(),
+          },
         },
-      },
-    });
-    if (!clearOtp) {
-      throw new RpcException(new BadGatewayException("Gagal menghapus OTP"));
+      });
+      if (!clearOtp) {
+        throw new BadGatewayException("Gagal menghapus OTP");
+      }
+    } catch (error) {
+      throw new RpcException(errorMappings(error));
     }
   }
 
   async verifyOtp(payload: TVerifyOtpRequest): Promise<TVerifyOtpResponse> {
-    await this.clearOtp();
-    const user = await this.getUserByEmail(payload);
-    const isVerified = user.email === payload.email && user.otp === payload.otp;
-    if (!isVerified) {
-      throw new RpcException(new UnauthorizedException("Email atau OTP tidak valid"));
-    }
+    try {
+      await this.clearOtp();
+      const user = await this.getUserByEmail(payload);
+      const isVerified = user.email === payload.email && user.otp === payload.otp;
+      if (!isVerified) {
+        throw new UnauthorizedException("Email atau OTP tidak valid");
+      }
 
-    const updateUser = await this.prisma.users.update({
-      where: {
-        email: payload.email,
-      },
-      data: {
-        isVerified: true,
-      },
-    });
+      const updateUser = await this.prisma.users.update({
+        where: {
+          email: payload.email,
+        },
+        data: {
+          isVerified: true,
+        },
+      });
 
-    if (!updateUser) {
-      throw new RpcException(new BadRequestException("Gagal verifikasi OTP"));
+      if (!updateUser) {
+        throw new BadRequestException("Gagal verifikasi OTP");
+      }
+      return {
+        message: "Berhasil verifikasi OTP",
+      };
+    } catch (error) {
+      throw new RpcException(errorMappings(error));
     }
-    return {
-      message: "Berhasil verifikasi OTP",
-    };
   }
 
   async forgotPassword(payload: TForgotPasswordRequest) {
-    await this.clearOtp();
-    const user = await this.getUserByEmail(payload);
-    if (!user) {
-      throw new RpcException(new NotFoundException("Email tidak ditemukan"));
+    try {
+      await this.clearOtp();
+      const user = await this.getUserByEmail(payload);
+      if (!user) {
+        throw new NotFoundException("Email tidak ditemukan");
+      }
+      return user;
+    } catch (error) {
+      throw new RpcException(errorMappings(error));
     }
-    return user;
   }
 
   async verifyOtpPassword(payload: TVerifyOtpPasswordRequest): Promise<TVerifyOtpPasswordResponse> {
-    await this.clearOtp();
-    const user = await this.getUserByEmail(payload);
-    const isVerified = user.email === payload.email && user.otp === payload.otp;
-    if (!isVerified) {
-      throw new RpcException(new UnauthorizedException("Email atau OTP tidak valid"));
-    }
+    try {
+      await this.clearOtp();
+      const user = await this.getUserByEmail(payload);
+      const isVerified = user.email === payload.email && user.otp === payload.otp;
+      if (!isVerified) {
+        throw new UnauthorizedException("Email atau OTP tidak valid");
+      }
 
-    return {
-      message: "Berhasil verifikasi OTP",
-    };
+      return {
+        message: "Berhasil verifikasi OTP",
+      };
+    } catch (error) {
+      throw new RpcException(errorMappings(error));
+    }
   }
 
   async resetPassword(payload: TResetPasswordRequest): Promise<TResetPasswordResponse> {
-    const newPassword = await encryptPassword(payload.password);
+    try {
+      const newPassword = await encryptPassword(payload.password);
 
-    const isEmailExist = await this.prisma.users.findUnique({
-      where: {
-        email: payload.email,
-      },
-    });
+      const isEmailExist = await this.prisma.users.findUnique({
+        where: {
+          email: payload.email,
+        },
+      });
 
-    if (!isEmailExist) {
-      throw new RpcException(new NotFoundException("Email tidak ditemukan"));
+      if (!isEmailExist) {
+        throw new NotFoundException("Email tidak ditemukan");
+      }
+
+      const user = await this.prisma.users.update({
+        where: {
+          email: payload.email,
+        },
+        data: {
+          password: newPassword,
+        },
+      });
+
+      if (!user) {
+        throw new BadRequestException("Gagal mengganti password");
+      }
+      return {
+        message: "Berhasil mengganti password",
+      };
+    } catch (error) {
+      throw new RpcException(errorMappings(error));
     }
-
-    const user = await this.prisma.users.update({
-      where: {
-        email: payload.email,
-      },
-      data: {
-        password: newPassword,
-      },
-    });
-
-    if (!user) {
-      throw new RpcException(new BadRequestException("Gagal mengganti password"));
-    }
-    return {
-      message: "Berhasil mengganti password",
-    };
   }
 }
